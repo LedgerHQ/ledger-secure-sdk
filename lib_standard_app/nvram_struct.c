c96046132efe07f710e6e45844ebf4fbd064057f
Nicolas Roggeman <nicolas.roggeman@ledger.fr>
2024-03-26 14:58:20 +0100

    Improve Nvram version management & add Nvram properties

--- lib_standard_app/nvram_struct.c
+++ lib_standard_app/nvram_struct.c
@@ -12,16 +12,22 @@
 const Nvram_t N_nvram_real;
 
 /**
- * @brief init header of NVRAM structure (set "NVRA" tag)
+ * @brief init header of NVRAM structure :
+ *  - set "NVRA" tag
+ *  - set size
+ *  - set struct and data versions
+ *  - set properties
+ * @param data_version Version of the data
  */
-void nvram_init(void)
+void nvram_init(uint32_t data_version)
 {
     Nvram_header_t header;
 
     memcpy(header.tag, (void *) "NVRA", 4);
-    // NVRAM_STRUCT_VERSION and NVRAM_DATA_VERSION must be defined in nvram_data.h
+    // NVRAM_STRUCT_VERSION and NVRAM_DATA_PROPERTIES must be defined in nvram_data.h
     header.struct_version = NVRAM_STRUCT_VERSION;
-    header.data_version   = NVRAM_DATA_VERSION;
+    header.data_version   = data_version;
+    header.properties     = NVRAM_DATA_PROPERTIES;
     header.size           = sizeof(Nvram_data_t);
     nvm_write((void *) &N_nvram.header, (void *) &header, sizeof(Nvram_header_t));
 }
@@ -38,15 +44,23 @@ uint32_t nvram_get_size(void)
 /**
  * @brief get the version of app data structure
  */
-uint8_t nvram_get_struct_version(void)
+uint16_t nvram_get_struct_version(void)
 {
     return N_nvram.header.struct_version;
 }
 
 /**
- * @brief get the version of app data data
+ * @brief get the version of app data
  */
-uint8_t nvram_get_data_version(void)
+uint32_t nvram_get_data_version(void)
+{
+    return N_nvram.header.data_version;
+}
+
+/**
+ * @brief get the properties of app data
+ */
+uint16_t nvram_get_properties(void)
 {
     return N_nvram.header.data_version;
 }
@@ -63,6 +77,14 @@ bool nvram_is_initalized(void)
         return false;
     }
     return true;
+}
+
+/**
+ * @brief set data version of app data
+ */
+void nvram_set_data_version(uint32_t data_version)
+{
+    nvm_write((void *) &N_nvram.header.data_version, (void *) &data_version, sizeof(uint32_t));
 }
 
 #endif  // HAVE_APP_NVRAM
