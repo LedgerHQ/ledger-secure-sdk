63b222c00d2b6bac018c0c965d64163ede27440a
Nicolas Roggeman <nicolas.roggeman@ledger.fr>
2024-03-26 14:58:20 +0100

    Improve Nvram version management & add Nvram properties

--- include/nvram_struct.h
+++ include/nvram_struct.h
@@ -13,15 +13,22 @@
 #include "nvram_data.h"
 #endif  // HAVE_BOLOS
 
+///< bit to indicate in properties that the NVRAM contains settings
+#define CONTAINS_SETTINGS       (1 << 0)
+///< bit to indicate in properties that the NVRAM contains sensitive data
+#define CONTAINS_SENSITIVE_DATA (1 << 1)
+
 /**
  * @brief Structure defining the header of NVRAM
  *
  */
 typedef struct Nvram_header_s {
     char     tag[4];          ///< ['N','V','R','A'] array, when properly initialized
-    uint32_t size;            ///< size in bytes of the data
-    uint32_t struct_version;  ///< version of the structure of data (to be set once at first
+    uint32_t size;            ///< size in bytes of the data (Nvram_data_t structure)
+    uint16_t struct_version;  ///< version of the structure of data (to be set once at first
                               ///< application start-up)
+    uint16_t properties;      ///< used as a bitfield to set properties, like: contains settings,
+                              ///< contains sensitive data
     uint32_t data_version;    ///< version of the content of data (to be updated every time data are
                               ///< updated)
 } Nvram_header_t;
@@ -51,10 +58,12 @@ extern const Nvram_t N_nvram_real;
  */
 #define N_nvram (*(volatile Nvram_t *) PIC(&N_nvram_real))
 
-extern void     nvram_init(void);
-extern uint32_t nvram_get_size(void);
-extern uint8_t  nvram_get_struct_version(void);
-extern uint8_t  nvram_get_data_version(void);
-extern bool     nvram_is_initalized(void);
+void     nvram_init(uint32_t data_version);
+uint32_t nvram_get_size(void);
+uint16_t nvram_get_struct_version(void);
+uint16_t nvram_get_properties(void);
+uint32_t nvram_get_data_version(void);
+bool     nvram_is_initalized(void);
+void     nvram_set_data_version(uint32_t data_version);
 
 #endif  // HAVE_BOLOS
