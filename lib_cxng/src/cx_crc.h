2215ee525e710a65a5278a7899abfdd481d27ee8
Xavier Chapron <xavier.chapron@ledger.fr>
2024-04-04 17:03:46 +0200

    lib_cxng: Align CRC32 include on CRC16

--- lib_cxng/src/cx_crc.h
+++ lib_cxng/src/cx_crc.h
@@ -21,14 +21,7 @@
 
 #ifdef HAVE_CRC
 
-#include <stddef.h>
-#include <stdint.h>
-
-/** CRC32 initial value */
-#define CX_CRC32_INIT 0xFFFFFFFF
-
-uint32_t cx_crc32(const void *buf, size_t len);
-uint32_t cx_crc32_update(uint32_t crc_state, const void *buf, size_t len);
+#include "lcx_crc.h"
 
 #endif  // HAVE_CRC
 
