63bab0746047ff5374a0c684957267edcf80ce77
Daniel Morais <daniel.morais@ledger.fr>
2024-04-08 13:57:35 +0200

    Remove warning 'SPRINTF redefined'

--- include/os_print.h
+++ include/os_print.h
@@ -4,6 +4,9 @@
 #include "app_config.h"
 
 // avoid typing the size each time
+#ifdef SPRINTF
+#undef SPRINTF
+#endif  // SPRINTF
 #define SPRINTF(strbuf, ...) snprintf(strbuf, sizeof(strbuf), __VA_ARGS__)
 
 #ifdef HAVE_PRINTF
