7cf169e62ffcfc045d0cf92abfe8fb2d651e2884
Francois Beutin <francois.beutin@ledger.fr>
2024-05-13 10:32:04 +0200

    Clean unified repository before nanos-secure-sdk import

--- target/fatstacks/include/bolos_target.h
+++
@@ -1,5 +0,0 @@
-#ifndef ST33K1M5
-  #define ST33K1M5
-#endif // ST33K1M5
-#define TARGET_ID 0x33200004
-#define TARGET_FATSTACKS
--- target/nanox/include/bolos_target.h
+++
@@ -1,5 +0,0 @@
-#ifndef ST33
-  #define ST33
-#endif // ST33
-#define TARGET_ID 0x33000004
-#define TARGET_NANOX
