fd95dac38da2b73bed37186f327adf4ebfad4f5c
sra <sandra.rasoamiaramanana@ledger.fr>
2024-05-22 13:30:37 +0200

    Add D2D restore status

--- include/os_seed.h
+++ include/os_seed.h
@@ -88,7 +88,8 @@ enum {
     ONBOARDING_STATUS_CHOOSE_NAME,
     ONBOARDING_STATUS_RECOVER_RESTORE_SEED,
     ONBOARDING_STATUS_SETUP_CHOICE_RESTORE_SEED,
-    ONBOARDING_STATUS_CHECKING
+    ONBOARDING_STATUS_CHECKING,
+    ONBOARDING_STATUS_D2D_RESTORE
 };
 SYSCALL void os_perso_set_onboarding_status(unsigned int state,
                                             unsigned int count,
