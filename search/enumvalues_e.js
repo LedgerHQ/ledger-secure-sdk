e28ed3b1dd02e191e2842a991c5dc0a470117b76
cedelavergne-ledger <cedelavergne-ledger@users.noreply.github.com>
2024-09-06 16:20:34 +0000

    deploy: 8706d88376cbdd52392e6f25456d55c80c13ca5d

--- search/enumvalues_e.js
+++ search/enumvalues_e.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['qr_5fcode_3825',['QR_CODE',['../nbgl__types_8h.html#aabda6232106281a9b84f3464fb43f8a7a9a922d054e1cd93d549c59b4862dc52e',1,'nbgl_types.h']]],
-  ['qrcode_5fv10_3826',['QRCODE_V10',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5ea33057fff9b323ff98bad2607e11a68c5',1,'nbgl_types.h']]],
-  ['qrcode_5fv4_3827',['QRCODE_V4',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5eaaba7b99e456d78a55db9e814b3790cd5',1,'nbgl_types.h']]],
-  ['qrcode_5fv4_5fsmall_3828',['QRCODE_V4_SMALL',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5ea98744275ffca217fc8f07722dac6f75c',1,'nbgl_types.h']]],
-  ['quit_5fapp_5ftext_3829',['QUIT_APP_TEXT',['../nbgl__page_8h.html#afb48cdeef2dbc001c1091fdfc2c95d3da0a1f25f93a03429f08a0892f9fc8a2e9',1,'nbgl_page.h']]],
-  ['quit_5ficon_3830',['QUIT_ICON',['../nbgl__page_8h.html#afb48cdeef2dbc001c1091fdfc2c95d3da4d91c622c0c89a27564d1302b7d2b3fc',1,'nbgl_page.h']]]
+  ['qr_5fcode_3829',['QR_CODE',['../nbgl__types_8h.html#aabda6232106281a9b84f3464fb43f8a7a9a922d054e1cd93d549c59b4862dc52e',1,'nbgl_types.h']]],
+  ['qrcode_5fv10_3830',['QRCODE_V10',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5ea33057fff9b323ff98bad2607e11a68c5',1,'nbgl_types.h']]],
+  ['qrcode_5fv4_3831',['QRCODE_V4',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5eaaba7b99e456d78a55db9e814b3790cd5',1,'nbgl_types.h']]],
+  ['qrcode_5fv4_5fsmall_3832',['QRCODE_V4_SMALL',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5ea98744275ffca217fc8f07722dac6f75c',1,'nbgl_types.h']]],
+  ['quit_5fapp_5ftext_3833',['QUIT_APP_TEXT',['../nbgl__page_8h.html#afb48cdeef2dbc001c1091fdfc2c95d3da0a1f25f93a03429f08a0892f9fc8a2e9',1,'nbgl_page.h']]],
+  ['quit_5ficon_3834',['QUIT_ICON',['../nbgl__page_8h.html#afb48cdeef2dbc001c1091fdfc2c95d3da4d91c622c0c89a27564d1302b7d2b3fc',1,'nbgl_page.h']]]
 ];
