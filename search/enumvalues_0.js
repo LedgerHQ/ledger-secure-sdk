116c044b76919c54f5dea4f0688d3ff7c8ad5b29
abonnaudet-ledger <abonnaudet-ledger@users.noreply.github.com>
2024-10-30 10:20:40 +0000

    deploy: 870e1a979763127b9e5e520ca6af1b5a3ce0788c

--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['address_5fbook_5falias_3628',['ADDRESS_BOOK_ALIAS',['../nbgl__content_8h.html#aa4402a878699462ea2897df1048f16ebace2c6317f384f361441cdebbf3260baf',1,'nbgl_content.h']]],
-  ['app_5flogger_3629',['APP_LOGGER',['../nbgl__debug_8h.html#adc29c2ff13d900c2f185ee95427fb06ca53109ee21c43dd0d246e86d9f0aa30cb',1,'nbgl_debug.h']]]
+  ['address_5fbook_5falias_3651',['ADDRESS_BOOK_ALIAS',['../nbgl__content_8h.html#aa4402a878699462ea2897df1048f16ebace2c6317f384f361441cdebbf3260baf',1,'nbgl_content.h']]],
+  ['app_5fctx_3652',['APP_CTX',['../nbgl__touch_8c.html#a394b3903fbf00ba2b6243f60689a5a5fa3a7df9667f9b767239268afa893fa06c',1,'nbgl_touch.c']]],
+  ['app_5flogger_3653',['APP_LOGGER',['../nbgl__debug_8h.html#adc29c2ff13d900c2f185ee95427fb06ca53109ee21c43dd0d246e86d9f0aa30cb',1,'nbgl_debug.h']]]
 ];
--- search/files_4.js
+++ search/files_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['io_2ec_2461',['io.c',['../io_8c.html',1,'']]],
-  ['io_2eh_2462',['io.h',['../io_8h.html',1,'']]]
+  ['io_2ec_2475',['io.c',['../io_8c.html',1,'']]],
+  ['io_2eh_2476',['io.h',['../io_8h.html',1,'']]]
 ];
