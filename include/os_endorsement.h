d320287796e115cd85298783a5ad0a6108ac8783
Arthur Bonnaudet <arthur.bonnaudet@ledger.fr>
2024-11-07 10:02:19 +0100

    Refactoring in endorsement related syscalls

--- include/os_endorsement.h
+++ include/os_endorsement.h
@@ -7,13 +7,6 @@
 /* -                         ENDORSEMENT FEATURE                         - */
 /* ----------------------------------------------------------------------- */
 
-typedef enum endorsement_revoke_id_e {
-    ENDORSEMENT_REVOKE_ID_ALL   = 0,
-    ENDORSEMENT_REVOKE_ID_SLOT1 = 1,
-    ENDORSEMENT_REVOKE_ID_SLOT2 = 2,
-    ENDORSEMENT_REVOKE_ID_LAST  = 3,
-} endorsement_revoke_id_t;
-
 #define ENDORSEMENT_MAX_ASN1_LENGTH (1 + 1 + 2 * (1 + 1 + 33))
 
 SYSCALL unsigned int os_endorsement_get_code_hash(unsigned char *buffer PLENGTH(32));
@@ -40,7 +33,3 @@ SYSCALL unsigned int os_endorsement_key2_derive_sign_data(unsigned char *src PLE
 
 SYSCALL unsigned int os_endorsement_get_metadata(unsigned char         index,
                                                  unsigned char *buffer PLENGTH(8));
-
-SYSCALL void os_endorsement_revoke_slot1(void);
-SYSCALL void os_endorsement_revoke_slot2(void);
-SYSCALL void os_endorsement_revoke_all(void);
