36125b6f1a3b6caaaee7cfa28d0d9adb04090bbc
dmorais-ledger <dmorais-ledger@users.noreply.github.com>
2024-11-13 12:20:28 +0000

    deploy: 8f2486f173585aec93e4607e6df87e5f4189bdb9

--- search/typedefs_1.js
+++ search/typedefs_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['bagl_5felement_5fcallback_5ft_3504',['bagl_element_callback_t',['../ux__bagl_8h.html#a62599d1b1a01cdb35025638f13d3be70',1,'ux_bagl.h']]],
-  ['bagl_5felement_5ft_3505',['bagl_element_t',['../ux__bagl_8h.html#a45f9ac7dba48418bdc6f5d03c356eb40',1,'ux_bagl.h']]],
-  ['button_5fpush_5fcallback_5ft_3506',['button_push_callback_t',['../ux__bagl_8h.html#abec57042d81077b2bf9a4eb420d7555f',1,'ux_bagl.h']]]
+  ['bagl_5felement_5fcallback_5ft_3472',['bagl_element_callback_t',['../ux__bagl_8h.html#a62599d1b1a01cdb35025638f13d3be70',1,'ux_bagl.h']]],
+  ['bagl_5felement_5ft_3473',['bagl_element_t',['../ux__bagl_8h.html#a45f9ac7dba48418bdc6f5d03c356eb40',1,'ux_bagl.h']]],
+  ['button_5fpush_5fcallback_5ft_3474',['button_push_callback_t',['../ux__bagl_8h.html#abec57042d81077b2bf9a4eb420d7555f',1,'ux_bagl.h']]]
 ];
--- search/typedefs_9.js
+++ search/typedefs_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['onimagedrawcallback_5ft_3583',['onImageDrawCallback_t',['../nbgl__obj_8h.html#ac41c144cbf4db3e195a03210f1157cde',1,'nbgl_obj.h']]],
-  ['ontextdrawcallback_5ft_3584',['onTextDrawCallback_t',['../nbgl__obj_8h.html#a944ddf64be85626aa2c7acf339cbc6e2',1,'nbgl_obj.h']]]
+  ['onimagedrawcallback_5ft_3550',['onImageDrawCallback_t',['../nbgl__obj_8h.html#ac41c144cbf4db3e195a03210f1157cde',1,'nbgl_obj.h']]],
+  ['ontextdrawcallback_5ft_3551',['onTextDrawCallback_t',['../nbgl__obj_8h.html#a944ddf64be85626aa2c7acf339cbc6e2',1,'nbgl_obj.h']]]
 ];
