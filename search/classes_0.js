de197eb8e3459ca104caef05d99d03e0ba6c9aa7
nroggeman-ledger <nroggeman-ledger@users.noreply.github.com>
2025-01-02 09:12:13 +0000

    deploy: a2be76cb8a7cf66079ad37101c33853a3278d2d3

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['bagl_5felement_5fe_2275',['bagl_element_e',['../structbagl__element__e.html',1,'']]],
-  ['ble_5fconnection_5ft_2276',['ble_connection_t',['../structble__connection__t.html',1,'']]],
-  ['buffer_5ft_2277',['buffer_t',['../structbuffer__t.html',1,'']]]
+  ['bagl_5felement_5fe_2315',['bagl_element_e',['../structbagl__element__e.html',1,'']]],
+  ['ble_5fconnection_5ft_2316',['ble_connection_t',['../structble__connection__t.html',1,'']]],
+  ['buffer_5ft_2317',['buffer_t',['../structbuffer__t.html',1,'']]]
 ];
--- search/files_6.js
+++ search/files_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['macros_2eh_2496',['macros.h',['../macros_8h.html',1,'']]],
-  ['main_2ec_2497',['main.c',['../main_8c.html',1,'']]],
-  ['mainpage_2edox_2498',['mainpage.dox',['../doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__cxng_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__nbgl_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__blewbxx__impl_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__nfc_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__standard__app_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__stusb_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__stusb__impl_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__u2f_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__ux_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__ux__nbgl_2doc_2mainpage_8dox.html',1,'(Global Namespace)']]],
-  ['mainpage_5fnanos_2edox_2499',['mainpage_nanos.dox',['../mainpage__nanos_8dox.html',1,'']]]
+  ['macros_2eh_2539',['macros.h',['../macros_8h.html',1,'']]],
+  ['main_2ec_2540',['main.c',['../main_8c.html',1,'']]],
+  ['mainpage_2edox_2541',['mainpage.dox',['../doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__cxng_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__nbgl_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__blewbxx__impl_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__nfc_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__standard__app_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__stusb_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__stusb__impl_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__u2f_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__ux_2doc_2mainpage_8dox.html',1,'(Global Namespace)'],['../lib__ux__nbgl_2doc_2mainpage_8dox.html',1,'(Global Namespace)']]],
+  ['mainpage_5fnanos_2edox_2542',['mainpage_nanos.dox',['../mainpage__nanos_8dox.html',1,'']]]
 ];
