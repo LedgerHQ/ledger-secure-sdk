0c5dd934e4ea1725be13afc0546d8a6cb26fd061
cedelavergne-ledger <cedelavergne-ledger@users.noreply.github.com>
2025-02-03 16:13:30 +0000

    deploy: 4d003bb4f7b0dfa2c39caa6feb1c0acc26c2a50f

--- search/enumvalues_6.js
+++ search/enumvalues_6.js
@@ -6,11 +6,12 @@ var searchData=
   ['hardware_5fswipe_5fup_3',['HARDWARE_SWIPE_UP',['../nbgl__types_8h.html#aa46b0267d63d4d7ce935d65571fb06f5af44e5cd4c3540079ea98afd35b16668c',1,'nbgl_types.h']]],
   ['header_5fback_5fand_5fprogress_4',['HEADER_BACK_AND_PROGRESS',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aa1c238c97367b2a81d3835decdb7e15d2',1,'nbgl_layout.h']]],
   ['header_5fback_5fand_5ftext_5',['HEADER_BACK_AND_TEXT',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aa98155fa3797a7ca12d107b3d8dbdc344',1,'nbgl_layout.h']]],
-  ['header_5fempty_6',['HEADER_EMPTY',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aa5cd35e7818c32b37a740e4622f0c39da',1,'nbgl_layout.h']]],
-  ['header_5fextended_5fback_7',['HEADER_EXTENDED_BACK',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aa71d2e9e742eb4eaafd9eee8a086c34f8',1,'nbgl_layout.h']]],
-  ['header_5findex_8',['HEADER_INDEX',['../nbgl__layout__internal_8h.html#a458e651af6690959efa2afb96be7d609a8b95510e5598b54516669f8f630c58d8',1,'nbgl_layout_internal.h']]],
-  ['header_5fright_5ftext_9',['HEADER_RIGHT_TEXT',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aaa5a21a56ce492b550be922310ef733ed',1,'nbgl_layout.h']]],
-  ['header_5ftitle_10',['HEADER_TITLE',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aa18ca44b6d4c4edf3c14b4a4f2683acdb',1,'nbgl_layout.h']]],
-  ['horizontal_11',['HORIZONTAL',['../nbgl__types_8h.html#a45c8941c6bef8b2e44b38f1c66c1a5d8a4dd51ad73508d6fc83a502966779e48e',1,'nbgl_types.h']]],
-  ['horizontal_5fnav_12',['HORIZONTAL_NAV',['../nbgl__layout_8h.html#a2c0a3234a9015214367700ea41d45eedae7a28095c569a3f2ac639e852d6f9149',1,'nbgl_layout.h']]]
+  ['header_5fback_5ficon_5fand_5ftext_6',['HEADER_BACK_ICON_AND_TEXT',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aac12291ca5db59aedeb608b22b36bd6af',1,'nbgl_layout.h']]],
+  ['header_5fempty_7',['HEADER_EMPTY',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aa5cd35e7818c32b37a740e4622f0c39da',1,'nbgl_layout.h']]],
+  ['header_5fextended_5fback_8',['HEADER_EXTENDED_BACK',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aa71d2e9e742eb4eaafd9eee8a086c34f8',1,'nbgl_layout.h']]],
+  ['header_5findex_9',['HEADER_INDEX',['../nbgl__layout__internal_8h.html#a2970898e8a43ce21e1cc510d49f1b89da8b95510e5598b54516669f8f630c58d8',1,'nbgl_layout_internal.h']]],
+  ['header_5fright_5ftext_10',['HEADER_RIGHT_TEXT',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aaa5a21a56ce492b550be922310ef733ed',1,'nbgl_layout.h']]],
+  ['header_5ftitle_11',['HEADER_TITLE',['../nbgl__layout_8h.html#a9879d9ceabe625bd287de2af3ca3b52aa18ca44b6d4c4edf3c14b4a4f2683acdb',1,'nbgl_layout.h']]],
+  ['horizontal_12',['HORIZONTAL',['../nbgl__types_8h.html#a45c8941c6bef8b2e44b38f1c66c1a5d8a4dd51ad73508d6fc83a502966779e48e',1,'nbgl_types.h']]],
+  ['horizontal_5fnav_13',['HORIZONTAL_NAV',['../nbgl__layout_8h.html#a2c0a3234a9015214367700ea41d45eedae7a28095c569a3f2ac639e852d6f9149',1,'nbgl_layout.h']]]
 ];
