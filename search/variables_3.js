0aaccd321f184c65924a09f4a74341309bb2b258
dmorais-ledger <dmorais-ledger@users.noreply.github.com>
2025-03-18 16:10:27 +0000

    deploy: 0f3417ca328c8af249d442edd0c2b5fe5a0cc7ea

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,19 +1,16 @@
 var searchData=
 [
-  ['dappprovider_0',['dAppProvider',['../structnbgl__warning__t.html#a28dc405c61353028a3fe673d2173ec0f',1,'nbgl_warning_t']]],
-  ['data_1',['data',['../structcommand__t.html#ae96e210e6ca48af0664f0cd4bf19e4e5',1,'command_t']]],
-  ['dec_5ffunc_2',['dec_func',['../structcx__cipher__base__t.html#a3d6564e72d2979a4d8d68fc1ffd87053',1,'cx_cipher_base_t']]],
-  ['delayms_3',['delayMs',['../structnbgl__animation__s.html#a637094c5aafc3180096c0bd8972bad87',1,'nbgl_animation_s']]],
-  ['description_4',['description',['../structnbgl__contentCenter__t.html#a1908111efeebaaa84962496207967781',1,'nbgl_contentCenter_t']]],
-  ['destination_5faddress_5',['destination_address',['../structcreate__transaction__parameters__s.html#a8019df5402d0abcfd7155d99f4593ba1',1,'create_transaction_parameters_s']]],
-  ['destination_5faddress_5fextra_5fid_6',['destination_address_extra_id',['../structcreate__transaction__parameters__s.html#a49513ff8c5bd3b0f9e28987f3e69d7bf',1,'create_transaction_parameters_s']]],
-  ['details_7',['details',['../structnbgl__warningBarList__t.html#a429879f31b62fb969b6d80fbe384cf86',1,'nbgl_warningBarList_t']]],
-  ['detailsbuttonicon_8',['detailsbuttonicon',['../structnbgl__contentTagValueDetails__t.html#a721b1cc9a1ebd012367c4e35835d254d',1,'nbgl_contentTagValueDetails_t::detailsButtonIcon'],['../structnbgl__contentTagValueConfirm__t.html#ace3d086fb542f41893f9ee9381fc4869',1,'nbgl_contentTagValueConfirm_t::detailsButtonIcon']]],
-  ['detailsbuttontext_9',['detailsbuttontext',['../structnbgl__contentTagValueDetails__t.html#a8cfe3900f6763243bc353d1d9c93c67c',1,'nbgl_contentTagValueDetails_t::detailsButtonText'],['../structnbgl__contentTagValueConfirm__t.html#a861cf55885415db99e52c8882a57ed79',1,'nbgl_contentTagValueConfirm_t::detailsButtonText']]],
-  ['detailsbuttontoken_10',['detailsbuttontoken',['../structnbgl__contentTagValueDetails__t.html#a7f2ecdcffa41cbbc6931bb508c4690fa',1,'nbgl_contentTagValueDetails_t::detailsButtonToken'],['../structnbgl__contentTagValueConfirm__t.html#a3b21b4e7d9fb5cf2e4f9c4775107ca40',1,'nbgl_contentTagValueConfirm_t::detailsButtonToken']]],
-  ['device_5fname_11',['device_name',['../structledger__ble__data__t.html#ad2a72f2403547e5d3a4b9b3233b5bde5',1,'ledger_ble_data_t']]],
-  ['device_5fname_5flength_12',['device_name_length',['../structledger__ble__data__t.html#ad3cdbbb297c307eac85c4d97a015c3fa',1,'ledger_ble_data_t']]],
-  ['digitindexes_13',['digitIndexes',['../structnbgl__keypad__s.html#acffdb7ec8716055b6a0d4923d79656e6',1,'nbgl_keypad_s']]],
-  ['direction_14',['direction',['../structnbgl__layoutNavigation__t.html#a214dcdf6f75e7c4859969d4edaf16f2a',1,'nbgl_layoutNavigation_t::direction'],['../structnbgl__line__s.html#a09d4c73f9500e6f29f07c041099e64b8',1,'nbgl_line_s::direction']]],
-  ['doubletext_15',['doubleText',['../structnbgl__layoutFooter__t.html#ae5fe92e787e0a01a083c9d584b5e0d1c',1,'nbgl_layoutFooter_t']]]
+  ['data_0',['data',['../structcommand__t.html#ae96e210e6ca48af0664f0cd4bf19e4e5',1,'command_t']]],
+  ['dec_5ffunc_1',['dec_func',['../structcx__cipher__base__t.html#a3d6564e72d2979a4d8d68fc1ffd87053',1,'cx_cipher_base_t']]],
+  ['description_2',['description',['../structnbgl__contentCenter__t.html#a1908111efeebaaa84962496207967781',1,'nbgl_contentCenter_t']]],
+  ['destination_5faddress_3',['destination_address',['../structcreate__transaction__parameters__s.html#a8019df5402d0abcfd7155d99f4593ba1',1,'create_transaction_parameters_s']]],
+  ['destination_5faddress_5fextra_5fid_4',['destination_address_extra_id',['../structcreate__transaction__parameters__s.html#a49513ff8c5bd3b0f9e28987f3e69d7bf',1,'create_transaction_parameters_s']]],
+  ['detailsbuttonicon_5',['detailsbuttonicon',['../structnbgl__contentTagValueDetails__t.html#a721b1cc9a1ebd012367c4e35835d254d',1,'nbgl_contentTagValueDetails_t::detailsButtonIcon'],['../structnbgl__contentTagValueConfirm__t.html#ace3d086fb542f41893f9ee9381fc4869',1,'nbgl_contentTagValueConfirm_t::detailsButtonIcon']]],
+  ['detailsbuttontext_6',['detailsbuttontext',['../structnbgl__contentTagValueConfirm__t.html#a861cf55885415db99e52c8882a57ed79',1,'nbgl_contentTagValueConfirm_t::detailsButtonText'],['../structnbgl__contentTagValueDetails__t.html#a8cfe3900f6763243bc353d1d9c93c67c',1,'nbgl_contentTagValueDetails_t::detailsButtonText']]],
+  ['detailsbuttontoken_7',['detailsbuttontoken',['../structnbgl__contentTagValueDetails__t.html#a7f2ecdcffa41cbbc6931bb508c4690fa',1,'nbgl_contentTagValueDetails_t::detailsButtonToken'],['../structnbgl__contentTagValueConfirm__t.html#a3b21b4e7d9fb5cf2e4f9c4775107ca40',1,'nbgl_contentTagValueConfirm_t::detailsButtonToken']]],
+  ['device_5fname_8',['device_name',['../structledger__ble__data__t.html#ad2a72f2403547e5d3a4b9b3233b5bde5',1,'ledger_ble_data_t']]],
+  ['device_5fname_5flength_9',['device_name_length',['../structledger__ble__data__t.html#ad3cdbbb297c307eac85c4d97a015c3fa',1,'ledger_ble_data_t']]],
+  ['digitindexes_10',['digitIndexes',['../structnbgl__keypad__s.html#acffdb7ec8716055b6a0d4923d79656e6',1,'nbgl_keypad_s']]],
+  ['direction_11',['direction',['../structnbgl__layoutNavigation__t.html#a214dcdf6f75e7c4859969d4edaf16f2a',1,'nbgl_layoutNavigation_t::direction'],['../structnbgl__line__s.html#a09d4c73f9500e6f29f07c041099e64b8',1,'nbgl_line_s::direction']]],
+  ['doubletext_12',['doubleText',['../structnbgl__layoutFooter__t.html#ae9906f79f168eae0693d9972154fcc65',1,'nbgl_layoutFooter_t']]]
 ];
