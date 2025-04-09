c432ee1ecbe93384acca2a1e9bb437600d26ba33
nroggeman-ledger <nroggeman-ledger@users.noreply.github.com>
2025-04-09 14:19:58 +0000

    deploy: 8faedb18604b8e65d4c30ba0859437c98231701b

--- search/defines_1.js
+++ search/defines_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['action_5fand_5ffooter_5ffooter_5fheight_0',['ACTION_AND_FOOTER_FOOTER_HEIGHT',['../nbgl__layout_8c.html#a54bdc32bb6a6dbeb1f1efaa0c3c673f0',1,'nbgl_layout.c']]],
-  ['add64_1',['ADD64',['../cx__utils_8h.html#a4f15fb31bd7bd262691442e10af6d7e2',1,'cx_utils.h']]],
+  ['add64_0',['ADD64',['../cx__utils_8h.html#a4f15fb31bd7bd262691442e10af6d7e2',1,'cx_utils.h']]],
+  ['alloc_5fchunk_5fheader_5fsize_1',['ALLOC_CHUNK_HEADER_SIZE',['../mem__alloc_8c.html#a33e0cf3fef0f595a3fb9b97904a00b19',1,'mem_alloc.c']]],
   ['app_5fdescription_5fmax_5flen_2',['APP_DESCRIPTION_MAX_LEN',['../nbgl__use__case_8h.html#a8972d4a69d40e4efd9c7437a46d7c8b7',1,'nbgl_use_case.h']]],
   ['arch_5flittle_5fendian_3',['ARCH_LITTLE_ENDIAN',['../lcx__common_8h.html#a1c8ed4b72dac6929d882e2aeb0261081',1,'lcx_common.h']]],
   ['array_5flength_4',['ARRAY_LENGTH',['../macros_8h.html#a7539f9740177f669ce68a77ae64f38b5',1,'macros.h']]],
--- search/defines_f.js
+++ search/defines_f.js
@@ -1,11 +1,11 @@
 var searchData=
 [
   ['packed_5f_5f_0',['PACKED__',['../nbgl__types_8h.html#ad747f7e4cc6b92c71882be70b907ba64',1,'nbgl_types.h']]],
-  ['pic_5fbmp_1',['PIC_BMP',['../nbgl__fonts_8h.html#a1f8949d3ce6264afcdf8e27d4427e820',1,'nbgl_fonts.h']]],
-  ['pic_5fchar_2',['PIC_CHAR',['../nbgl__fonts_8h.html#a723810fb923700f83de76f45eaa6b87b',1,'nbgl_fonts.h']]],
-  ['pic_5ffont_3',['PIC_FONT',['../nbgl__fonts_8c.html#a9c273c8b135138abff8df69be805bf5a',1,'nbgl_fonts.c']]],
-  ['post_5fsubtext_5fmargin_4',['POST_SUBTEXT_MARGIN',['../nbgl__layout_8h.html#abd3dd2d58678134c63204083e64e9633',1,'nbgl_layout.h']]],
-  ['pre_5ftext_5fmargin_5',['PRE_TEXT_MARGIN',['../nbgl__layout_8h.html#a26601cb52ca49cdf5218b7f32d711796',1,'nbgl_layout.h']]],
-  ['privacy_5ficon_6',['PRIVACY_ICON',['../nbgl__obj_8h.html#acfa3b3d504d8402c230fc5e600b3ce9a',1,'nbgl_obj.h']]],
-  ['push_5ficon_7',['PUSH_ICON',['../nbgl__obj_8h.html#a0cb5a99737d5ac2fc7b206ddd40fc9ac',1,'nbgl_obj.h']]]
+  ['payload_5falignement_1',['PAYLOAD_ALIGNEMENT',['../mem__alloc_8c.html#a5107199c34f5f4b6c029cf2b5dc47bb5',1,'mem_alloc.c']]],
+  ['payload_5fdata_5falignement_2',['PAYLOAD_DATA_ALIGNEMENT',['../mem__alloc_8c.html#adc0eda6f62e7558b85a9fe60ebd50e2b',1,'mem_alloc.c']]],
+  ['pic_5fbmp_3',['PIC_BMP',['../nbgl__fonts_8h.html#a1f8949d3ce6264afcdf8e27d4427e820',1,'nbgl_fonts.h']]],
+  ['pic_5fchar_4',['PIC_CHAR',['../nbgl__fonts_8h.html#a723810fb923700f83de76f45eaa6b87b',1,'nbgl_fonts.h']]],
+  ['pic_5ffont_5',['PIC_FONT',['../nbgl__fonts_8c.html#a9c273c8b135138abff8df69be805bf5a',1,'nbgl_fonts.c']]],
+  ['progressbar_5fheight_6',['PROGRESSBAR_HEIGHT',['../nbgl__layout_8c.html#a15fde6bd19824fd452e3b79387173958',1,'nbgl_layout.c']]],
+  ['progressbar_5fwidth_7',['PROGRESSBAR_WIDTH',['../nbgl__layout_8c.html#acd21097cbf356497c15c0e2465b14356',1,'nbgl_layout.c']]]
 ];
