9476bbfb9d621eaf1b7560f4a5a31c703f480763
Nicolas Roggeman <nicolas.roggeman@ledger.fr>
2025-04-18 15:45:06 +0200

    Add real NanoText and NanoDisplay fonts for Apex

--- lib_nbgl/include/nbgl_layout.h
+++ lib_nbgl/include/nbgl_layout.h
@@ -98,7 +98,8 @@ extern "C" {
 #define SIMPLE_FOOTER_HEIGHT              64
 #define SMALL_CENTERING_HEADER            24
 #define MEDIUM_CENTERING_HEADER           40
-#define LONG_PRESS_BUTTON_HEIGHT          100
+#define MEDIUM_CENTERING_FOOTER           40
+#define LONG_PRESS_BUTTON_HEIGHT          88
 
 #define PRE_TEXT_MARGIN     20
 #define TEXT_SUBTEXT_MARGIN 10
