82c1192f59c9440410a4b3a508ca10a53f8b2d57
cedelavergne-ledger <cedelavergne-ledger@users.noreply.github.com>
2025-04-28 06:36:57 +0000

    deploy: 105fe50e20b27f062debe6efbed5973f76248a4b

--- search/defines_10.js
+++ search/defines_10.js
@@ -5,5 +5,8 @@ var searchData=
   ['qr_5fv10_5fnb_5fpix_5fsize_2',['QR_V10_NB_PIX_SIZE',['../nbgl__draw_8h.html#a28eee35753a23e61f04f6d06c90b49d6',1,'nbgl_draw.h']]],
   ['qr_5fv4_5fnb_5fpix_5fsize_3',['QR_V4_NB_PIX_SIZE',['../nbgl__draw_8h.html#ac6c02694f8b78c8b0967cbf80ce93ca9',1,'nbgl_draw.h']]],
   ['qrcode_4',['qrcode',['../nbgl__draw_8c.html#ac37758eda76fc20418bb37238d25f74f',1,'nbgl_draw.c']]],
-  ['qrdrawbuffer_5',['QrDrawBuffer',['../nbgl__draw_8c.html#a046c7e075934b74c4b01dc9e93c9d905',1,'nbgl_draw.c']]]
+  ['qrcode_5ficon_5',['QRCODE_ICON',['../nbgl__obj_8h.html#af2d9b41c85e2e77eb024d56a90861496',1,'nbgl_obj.h']]],
+  ['qrdrawbuffer_6',['QrDrawBuffer',['../nbgl__draw_8c.html#a046c7e075934b74c4b01dc9e93c9d905',1,'nbgl_draw.c']]],
+  ['question_5fcircle_5ficon_7',['QUESTION_CIRCLE_ICON',['../nbgl__obj_8h.html#a513f3ac02ffcdb3709f1deb31e8bcbf2',1,'nbgl_obj.h']]],
+  ['question_5ficon_8',['QUESTION_ICON',['../nbgl__obj_8h.html#ad99adf0aba9464e8b3ce2c5b2d77387d',1,'nbgl_obj.h']]]
 ];
