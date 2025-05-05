34058e6702cf99708aa41f9197f71b6e54e2c361
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-05 11:32:50 +0200

    Add generated_mocks in compilation

--- fuzzing/mock/mock.cmake
+++ fuzzing/mock/mock.cmake
@@ -3,12 +3,11 @@ include_guard()
 # Commented lines need os mock or depend of another function that needs a mock
 file(GLOB LIB_MOCK_SOURCES
     "${CMAKE_SOURCE_DIR}/mock/written/os_task.c"
-    #"${CMAKE_SOURCE_DIR}/mock/generated/syscalls_generated.c"
+    "${CMAKE_SOURCE_DIR}/mock/generated/syscalls_generated.c"
 )
 
 add_library(lib_mock ${LIB_MOCK_SOURCES})
-
-#target_include_directories(lib_mock PUBLIC ${CMAKE_SOURCE_DIR}/../${TARGET_DEVICE}/include/)
 target_include_directories(lib_mock PUBLIC ${CMAKE_SOURCE_DIR}/../include/)
 target_include_directories(lib_mock PUBLIC ${CMAKE_SOURCE_DIR}/mock/written/)
-
+target_include_directories(lib_mock PUBLIC ${CMAKE_SOURCE_DIR}/../target/${TARGET_DEVICE}/include/)
+target_include_directories(lib_mock PUBLIC ${CMAKE_SOURCE_DIR}/../lib_cxng/include/)
