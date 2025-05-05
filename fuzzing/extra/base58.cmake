40ed496ac28ffd6ca47c7048b361809ede96724e
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-05 17:31:42 +0200

    Add os.c to fuzz framework and improve mock_gen

--- fuzzing/extra/base58.cmake
+++ fuzzing/extra/base58.cmake
@@ -4,9 +4,9 @@ include_guard()
 include(${CMAKE_SOURCE_DIR}/libs/lib_standard_app.cmake)
 
 # Define the executable and its properties
-add_executable(fuzz_base58  ${CMAKE_SOURCE_DIR}/harness/fuzzer_base58.c)
-target_compile_options(fuzz_base58  PUBLIC ${COMPILATION_FLAGS})
-target_link_options(fuzz_base58  PUBLIC ${COMPILATION_FLAGS})
+add_executable(fuzz_base58 ${CMAKE_SOURCE_DIR}/harness/fuzzer_base58.c)
+target_compile_options(fuzz_base58 PUBLIC ${COMPILATION_FLAGS})
+target_link_options(fuzz_base58 PUBLIC ${COMPILATION_FLAGS})
 
 # Link with required libraries
-target_link_libraries(fuzz_base58  PUBLIC lib_standard_app)
+target_link_libraries(fuzz_base58 PUBLIC lib_standard_app)
