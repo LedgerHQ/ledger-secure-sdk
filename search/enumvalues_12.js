3f1cdd02200cf40e08d98565192d966a14f104b6
jarevalo-ledger <jarevalo-ledger@users.noreply.github.com>
2025-05-06 09:04:13 +0000

    deploy: 83af614b6507b7b9090dcd2cc37b2438852e55ee

--- search/enumvalues_12.js
+++ search/enumvalues_12.js
@@ -16,11 +16,12 @@ var searchData=
   ['u2f_5fwait_5fasynch_5freply_5fready_13',['U2F_WAIT_ASYNCH_REPLY_READY',['../u2f__service_8h.html#a7b5e4d8b8c2d1ad326bb7b116030fca0a7b74b97c6f59a11d2b84e23ca6736be0',1,'u2f_service.h']]],
   ['up_5ffooter_5fbutton_14',['UP_FOOTER_BUTTON',['../nbgl__layout_8h.html#a88a4bd82691a0a53afdca2dd2a173694a41bc1957dac62b36cf0702cbe92d4b2d',1,'nbgl_layout.h']]],
   ['up_5ffooter_5fhorizontal_5fbuttons_15',['UP_FOOTER_HORIZONTAL_BUTTONS',['../nbgl__layout_8h.html#a88a4bd82691a0a53afdca2dd2a173694af5434f7eb4d16d4ad856424141cdb900',1,'nbgl_layout.h']]],
-  ['up_5ffooter_5findex_16',['UP_FOOTER_INDEX',['../nbgl__layout__internal_8h.html#a2970898e8a43ce21e1cc510d49f1b89da2ac48822874804a760f2378cce0c8d54',1,'nbgl_layout_internal.h']]],
+  ['up_5ffooter_5findex_16',['UP_FOOTER_INDEX',['../nbgl__layout__internal_8h.html#a16685eea158879e41b101ca3634de462a2ac48822874804a760f2378cce0c8d54',1,'nbgl_layout_internal.h']]],
   ['up_5ffooter_5flong_5fpress_17',['UP_FOOTER_LONG_PRESS',['../nbgl__layout_8h.html#a88a4bd82691a0a53afdca2dd2a173694a522e4f4b8629f1ef1d3ff3bbd032d750',1,'nbgl_layout.h']]],
   ['up_5ffooter_5ftext_18',['UP_FOOTER_TEXT',['../nbgl__layout_8h.html#a88a4bd82691a0a53afdca2dd2a173694a0993668db9da7bf5ce70f36935f1d1e7',1,'nbgl_layout.h']]],
   ['up_5ffooter_5ftip_5fbox_19',['UP_FOOTER_TIP_BOX',['../nbgl__layout_8h.html#a88a4bd82691a0a53afdca2dd2a173694a5ec6f9349ded662cc7a3d15cf00bb2cb',1,'nbgl_layout.h']]],
   ['upper_5fcase_20',['UPPER_CASE',['../nbgl__obj_8h.html#a526042c25a10ab7633bf29bd1cca44ceae1e489b6536e7429c92851b95854c6cf',1,'nbgl_obj.h']]],
   ['use_5fcase_5flogger_21',['USE_CASE_LOGGER',['../nbgl__debug_8h.html#adc29c2ff13d900c2f185ee95427fb06ca2aba8ceddb55fc3eb020c5f2ac2f0bf5',1,'nbgl_debug.h']]],
-  ['ux_5flogger_22',['UX_LOGGER',['../nbgl__debug_8h.html#adc29c2ff13d900c2f185ee95427fb06caaf3e0fa47a6383268a17905c3b8027a8',1,'nbgl_debug.h']]]
+  ['ux_5fctx_22',['UX_CTX',['../nbgl__touch_8c.html#a1fb9092bcdeada2d206bdc74afbbe122aae71184f80165ed57f017de11cf1db2f',1,'nbgl_touch.c']]],
+  ['ux_5flogger_23',['UX_LOGGER',['../nbgl__debug_8h.html#adc29c2ff13d900c2f185ee95427fb06caaf3e0fa47a6383268a17905c3b8027a8',1,'nbgl_debug.h']]]
 ];
