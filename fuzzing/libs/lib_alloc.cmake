9e1b7e05670315d428a017e2d66eb4a1f5bf2d31
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-06 16:14:46 +0200

    Fix path problems with CMakeFiles from APPs

--- fuzzing/libs/lib_alloc.cmake
+++ fuzzing/libs/lib_alloc.cmake
@@ -1,10 +1,10 @@
 include_guard()
 
 # Commented lines need os mock or depend of another function that needs a mock
-file(GLOB LIB_ALLOC_SOURCES "${CMAKE_SOURCE_DIR}/../lib_alloc/mem_alloc.c")
+file(GLOB LIB_ALLOC_SOURCES "${BOLOS_SDK}/lib_alloc/mem_alloc.c")
 
 add_library(lib_alloc ${LIB_ALLOC_SOURCES})
-target_include_directories(lib_alloc PUBLIC ${CMAKE_SOURCE_DIR}/../include/)
-target_include_directories(lib_alloc PUBLIC ${CMAKE_SOURCE_DIR}/../lib_alloc/)
-target_include_directories(
-  lib_alloc PUBLIC ${CMAKE_SOURCE_DIR}/../target/${TARGET_DEVICE}/include)
+target_include_directories(lib_alloc PUBLIC ${BOLOS_SDK}/include/)
+target_include_directories(lib_alloc PUBLIC ${BOLOS_SDK}/lib_alloc/)
+target_include_directories(lib_alloc
+                           PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
