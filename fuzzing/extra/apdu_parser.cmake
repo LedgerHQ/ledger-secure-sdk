9726a8a995aec2952a5ade848ee99886f76ca8d0
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-06 16:14:46 +0200

    Fix path problems with CMakeFiles from APPs

--- fuzzing/extra/apdu_parser.cmake
+++ fuzzing/extra/apdu_parser.cmake
@@ -1,11 +1,11 @@
 include_guard()
 
 # Include required liraries
-include(${CMAKE_SOURCE_DIR}/libs/lib_standard_app.cmake)
+include(${BOLOS_SDK}/fuzzing/libs/lib_standard_app.cmake)
 
 # Define the executable and its properties
 add_executable(fuzz_apdu_parser
-               ${CMAKE_SOURCE_DIR}/harness/fuzzer_apdu_parser.c)
+               ${BOLOS_SDK}/fuzzing/harness/fuzzer_apdu_parser.c)
 target_compile_options(fuzz_apdu_parser PUBLIC ${COMPILATION_FLAGS})
 target_link_options(fuzz_apdu_parser PUBLIC ${COMPILATION_FLAGS})
 
