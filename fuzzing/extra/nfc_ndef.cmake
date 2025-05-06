3355a2798c19371a6e35617d959cf2007add079a
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-06 16:14:46 +0200

    Fix path problems with CMakeFiles from APPs
    (cherry picked from commit 9726a8a995aec2952a5ade848ee99886f76ca8d0)

--- fuzzing/extra/nfc_ndef.cmake
+++ fuzzing/extra/nfc_ndef.cmake
@@ -1,11 +1,11 @@
 include_guard()
 
-include(${CMAKE_SOURCE_DIR}/mock/mock.cmake)
+include(${BOLOS_SDK}/fuzzing/mock/mock.cmake)
 
 # Commented lines need os mock or depend of another function that needs a mock
-file(GLOB LIB_NFC_SOURCES "${CMAKE_SOURCE_DIR}/../lib_nfc/src/nfc_ndef.c")
+file(GLOB LIB_NFC_SOURCES "${BOLOS_SDK}/fuzzing/../lib_nfc/src/nfc_ndef.c")
 
 add_library(lib_nfc ${LIB_NFC_SOURCES})
-target_include_directories(lib_nfc PUBLIC ${CMAKE_SOURCE_DIR}/../include/)
+target_include_directories(lib_nfc PUBLIC ${BOLOS_SDK}/fuzzing/../include/)
 target_include_directories(lib_nfc
-                           PUBLIC ${CMAKE_SOURCE_DIR}/../lib_nfc/include/)
+                           PUBLIC ${BOLOS_SDK}/fuzzing/../lib_nfc/include/)
--- fuzzing/libs/lib_qrcode.cmake
+++ fuzzing/libs/lib_qrcode.cmake
@@ -1,11 +1,10 @@
 include_guard()
 
 # Commented lines need os mock or depend of another function that needs a mock
-file(GLOB LIB_QRCODE_SOURCES "${CMAKE_SOURCE_DIR}/../qrcode/src/qrcodegen.c")
+file(GLOB LIB_QRCODE_SOURCES "${BOLOS_SDK}/qrcode/src/qrcodegen.c")
 
 add_library(lib_qrcode ${LIB_QRCODE_SOURCES})
-target_include_directories(lib_qrcode PUBLIC ${CMAKE_SOURCE_DIR}/../include/)
+target_include_directories(lib_qrcode PUBLIC ${BOLOS_SDK}/include/)
+target_include_directories(lib_qrcode PUBLIC ${BOLOS_SDK}/qrcode/include/)
 target_include_directories(lib_qrcode
-                           PUBLIC ${CMAKE_SOURCE_DIR}/../qrcode/include/)
-target_include_directories(
-  lib_qrcode PUBLIC ${CMAKE_SOURCE_DIR}/../target/${TARGET_DEVICE}/include)
+                           PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
