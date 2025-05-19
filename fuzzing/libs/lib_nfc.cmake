e19b7a937a1daa95120fec16fcfde19c01f929bb
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-19 14:35:04 +0200

    Add libs folder with library cmake files

--- fuzzing/libs/lib_nfc.cmake
+++ fuzzing/libs/lib_nfc.cmake
@@ -1,8 +1,11 @@
 include_guard()
+include(${BOLOS_SDK}/fuzzing/macros/macros.cmake)
 
 # Commented lines need os mock or depend of another function that needs a mock
 file(GLOB LIB_NFC_SOURCES "${BOLOS_SDK}/lib_nfc/src/nfc_ndef.c")
 
-add_library(lib_nfc ${LIB_NFC_SOURCES})
-target_include_directories(lib_nfc PUBLIC ${BOLOS_SDK}/include/)
-target_include_directories(lib_nfc PUBLIC ${BOLOS_SDK}/lib_nfc/include/)
+add_library(nfc ${LIB_NFC_SOURCES})
+target_link_libraries(nfc PUBLIC macros)
+target_compile_options(nfc PUBLIC ${COMPILATION_FLAGS})
+target_include_directories(nfc PUBLIC ${BOLOS_SDK}/include/)
+target_include_directories(nfc PUBLIC ${BOLOS_SDK}/lib_nfc/include/)
--- fuzzing/libs/lib_qrcode.cmake
+++ fuzzing/libs/lib_qrcode.cmake
@@ -1,10 +1,13 @@
 include_guard()
+include(${BOLOS_SDK}/fuzzing/macros/macros.cmake)
 
 # Commented lines need os mock or depend of another function that needs a mock
 file(GLOB LIB_QRCODE_SOURCES "${BOLOS_SDK}/qrcode/src/qrcodegen.c")
 
-add_library(lib_qrcode ${LIB_QRCODE_SOURCES})
-target_include_directories(lib_qrcode PUBLIC ${BOLOS_SDK}/include/)
-target_include_directories(lib_qrcode PUBLIC ${BOLOS_SDK}/qrcode/include/)
-target_include_directories(lib_qrcode
+add_library(qrcode ${LIB_QRCODE_SOURCES})
+target_link_libraries(qrcode PUBLIC macros)
+target_compile_options(qrcode PUBLIC ${COMPILATION_FLAGS})
+target_include_directories(qrcode PUBLIC ${BOLOS_SDK}/include/)
+target_include_directories(qrcode PUBLIC ${BOLOS_SDK}/qrcode/include/)
+target_include_directories(qrcode
                            PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
