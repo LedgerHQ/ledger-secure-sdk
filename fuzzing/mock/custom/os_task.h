087c7800d06d4fa5a299d71cb7d65b1526551238
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-19 14:54:42 +0200

    Fix clang format

--- fuzzing/mock/custom/os_task.h
+++ fuzzing/mock/custom/os_task.h
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
 try_context_t *try_context_get(void);
 
 try_context_t *try_context_set(try_context_t *context);
--- fuzzing/mock/mock.cmake
+++ fuzzing/mock/mock.cmake
@@ -6,17 +6,18 @@ include(${BOLOS_SDK}/fuzzing/libs/lib_standard_app.cmake)
 include(${BOLOS_SDK}/fuzzing/libs/lib_cxng.cmake)
 
 # Commented lines need os mock or depend of another function that needs a mock
-file(GLOB LIB_MOCK_SOURCES 
-     "${BOLOS_SDK}/src/os.c" 
-     "${BOLOS_SDK}/src/ledger_assert.c"
-     "${BOLOS_SDK}/src/cx_wrappers.c"
-     "${BOLOS_SDK}/src/cx_hash_iovec.c"
-     "${BOLOS_SDK}/fuzzing/mock/custom/os_task.c"
-     "${BOLOS_SDK}/fuzzing/mock/custom/main.c"
-     "${BOLOS_SDK}/fuzzing/mock/custom/pic.c"
-     "${BOLOS_SDK}/fuzzing/mock/custom/custom_syscalls.c"
-     "${BOLOS_SDK}/fuzzing/mock/generated/generated_syscalls.c"
-)
+file(
+  GLOB
+  LIB_MOCK_SOURCES
+  "${BOLOS_SDK}/src/os.c"
+  "${BOLOS_SDK}/src/ledger_assert.c"
+  "${BOLOS_SDK}/src/cx_wrappers.c"
+  "${BOLOS_SDK}/src/cx_hash_iovec.c"
+  "${BOLOS_SDK}/fuzzing/mock/custom/os_task.c"
+  "${BOLOS_SDK}/fuzzing/mock/custom/main.c"
+  "${BOLOS_SDK}/fuzzing/mock/custom/pic.c"
+  "${BOLOS_SDK}/fuzzing/mock/custom/custom_syscalls.c"
+  "${BOLOS_SDK}/fuzzing/mock/generated/generated_syscalls.c")
 
 add_library(mock ${LIB_MOCK_SOURCES})
 target_link_libraries(mock PUBLIC macros cxng nbgl standard_app)
@@ -26,4 +27,5 @@ target_include_directories(mock PUBLIC ${BOLOS_SDK}/lib_standard_app/)
 target_include_directories(mock PUBLIC ${BOLOS_SDK}/include/)
 target_include_directories(mock PUBLIC ${BOLOS_SDK}/fuzzing/mock/written/)
 target_include_directories(mock PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/)
-target_include_directories(mock PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include/)
+target_include_directories(mock
+                           PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include/)
