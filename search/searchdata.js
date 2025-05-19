e7646e8f22efeb734c294b17090f07f2326ecc1a
iartemov-ledger <iartemov-ledger@users.noreply.github.com>
2025-05-19 10:07:08 +0000

    deploy: d282e23bb404c728f7a70b2a04f44cfd6b7ec64b

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,7 +1,7 @@
 var indexSectionsWithContent =
 {
   0: "12_abcdefghiklmnopqrstuvwxy",
-  1: "bcghlmnqru",
+  1: "bcghlmnqu",
   2: "abcfilmnoprsuvw",
   3: "_abcdfghiklmnorsuvw",
   4: "abcdefghiklmnopqrstuvwxy",
