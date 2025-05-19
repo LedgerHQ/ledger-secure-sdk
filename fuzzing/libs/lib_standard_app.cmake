26705b75837775f250a51c2e4fa3b2fed616c536
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-19 14:35:04 +0200

    Add libs folder with library cmake files
    (cherry picked from commit 74bf48dc2864cfa4b727ba1331860f0b50b7f0a1)

--- fuzzing/libs/lib_standard_app.cmake
+++ fuzzing/libs/lib_standard_app.cmake
@@ -1,4 +1,7 @@
 include_guard()
+include(${BOLOS_SDK}/fuzzing/macros/macros.cmake)
+include(${BOLOS_SDK}/fuzzing/libs/lib_cxng.cmake)
+include(${BOLOS_SDK}/fuzzing/mock/mock.cmake)
 
 # Commented lines need os mock or depend of another function that needs a mock
 file(
@@ -8,9 +11,10 @@ file(
   "${BOLOS_SDK}/lib_standard_app/bip32.c"
   "${BOLOS_SDK}/lib_standard_app/base58.c"
   "${BOLOS_SDK}/lib_standard_app/buffer.c"
-  # "${BOLOS_SDK}/lib_standard_app/crypto_helpers.c"
+  "${BOLOS_SDK}/lib_standard_app/crypto_helpers.c"
   "${BOLOS_SDK}/lib_standard_app/format.c"
-  # "${BOLOS_SDK}/lib_standard_app/main.c" "${BOLOS_SDK}/lib_standard_app/io.c"
+  #"${BOLOS_SDK}/lib_standard_app/main.c"
+  "${BOLOS_SDK}/lib_standard_app/io.c"
   "${BOLOS_SDK}/lib_standard_app/parser.c"
   "${BOLOS_SDK}/lib_standard_app/swap_error_code_helpers.c"
   "${BOLOS_SDK}/lib_standard_app/swap_utils.c"
@@ -18,12 +22,11 @@ file(
   "${BOLOS_SDK}/lib_standard_app/write.c"
   "${BOLOS_SDK}/lib_standard_app/read.c")
 
-add_library(lib_standard_app ${LIB_STANDARD_APP_SOURCES})
-target_include_directories(lib_standard_app INTERFACE ${BOLOS_SDK}/include/)
-target_include_directories(
-  lib_standard_app INTERFACE ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
-target_include_directories(lib_standard_app INTERFACE ${BOLOS_SDK}/lib_cxng)
-target_include_directories(lib_standard_app
-                           INTERFACE ${BOLOS_SDK}/lib_cxng/include)
-target_include_directories(lib_standard_app
-                           INTERFACE ${BOLOS_SDK}/lib_standard_app)
+add_library(standard_app ${LIB_STANDARD_APP_SOURCES})
+target_link_libraries(standard_app PUBLIC macros mock cxng)
+target_compile_options(standard_app PUBLIC ${COMPILATION_FLAGS})
+target_include_directories(standard_app PUBLIC ${BOLOS_SDK}/include/)
+target_include_directories(standard_app PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
+target_include_directories(standard_app PUBLIC ${BOLOS_SDK}/lib_cxng)
+target_include_directories(standard_app PUBLIC ${BOLOS_SDK}/lib_cxng/include)
+target_include_directories(standard_app PUBLIC ${BOLOS_SDK}/lib_standard_app)
