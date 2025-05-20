b0caffd3ec65281d8f70103c7055c5cdd98c6c5c
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-20 17:52:18 +0200

    Add note about order priority for weak functions

--- fuzzing/mock/mock.cmake
+++ fuzzing/mock/mock.cmake
@@ -1,16 +1,16 @@
 # -----------------------------------------------------------------------------------
 # IMPORTANT NOTE:
 #
-# When globbing source files from the mock directory, it is **critical** that the
-# "custom" folder appears **alphabetically before** the "generated" folder.
+# When globbing source files from the mock directory, it is **critical** that
+# the "custom" folder appears **alphabetically before** the "generated" folder.
 #
-# This is because the symbols are **statically linked**, and the linker
-# resolves symbols in the **order they appear** in the source list. By ensuring that
-# the custom implementations come first, we allow them to **override** the
-# symbols from the generated mocks.
+# This is because the symbols are **statically linked**, and the linker resolves
+# symbols in the **order they appear** in the source list. By ensuring that the
+# custom implementations come first, we allow them to **override** the symbols
+# from the generated mocks.
 #
-# Failing to maintain this order may result in the linker choosing the wrong symbol,
-# and using the generated mocks instead of the custom ones.
+# Failing to maintain this order may result in the linker choosing the wrong
+# symbol, and using the generated mocks instead of the custom ones.
 # -----------------------------------------------------------------------------------
 include_guard()
 
@@ -32,10 +32,11 @@ file(
 add_library(mock ${LIB_MOCK_SOURCES})
 target_link_libraries(mock PUBLIC macros cxng nbgl standard_app)
 target_compile_options(mock PUBLIC ${COMPILATION_FLAGS})
-target_include_directories(mock PUBLIC ${BOLOS_SDK}/lib_cxng/include/
-                                        ${BOLOS_SDK}/target/${TARGET_DEVICE}/include/
-                                        ${BOLOS_SDK}/lib_standard_app/
-                                        ${BOLOS_SDK}/include/
-                                        ${BOLOS_SDK}/fuzzing/mock/written/
-                                        ${BOLOS_SDK}/target/${TARGET_DEVICE}/
-)
+target_include_directories(
+  mock
+  PUBLIC ${BOLOS_SDK}/lib_cxng/include/
+         ${BOLOS_SDK}/target/${TARGET_DEVICE}/include/
+         ${BOLOS_SDK}/lib_standard_app/
+         ${BOLOS_SDK}/include/
+         ${BOLOS_SDK}/fuzzing/mock/written/
+         ${BOLOS_SDK}/target/${TARGET_DEVICE}/)
