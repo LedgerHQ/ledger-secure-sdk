10f78ef1c560efa89d38f99c04b3bd5913446a97
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-20 17:46:36 +0200

    Assemble includes and use regex in globs in .cmake

--- fuzzing/libs/lib_qrcode.cmake
+++ fuzzing/libs/lib_qrcode.cmake
@@ -1,13 +1,11 @@
 include_guard()
 include(${BOLOS_SDK}/fuzzing/macros/macros.cmake)
 
-# Commented lines need os mock or depend of another function that needs a mock
-file(GLOB LIB_QRCODE_SOURCES "${BOLOS_SDK}/qrcode/src/qrcodegen.c")
+file(GLOB LIB_QRCODE_SOURCES "${BOLOS_SDK}/qrcode/src/*.c")
 
 add_library(qrcode ${LIB_QRCODE_SOURCES})
 target_link_libraries(qrcode PUBLIC macros)
 target_compile_options(qrcode PUBLIC ${COMPILATION_FLAGS})
-target_include_directories(qrcode PUBLIC ${BOLOS_SDK}/include/)
-target_include_directories(qrcode PUBLIC ${BOLOS_SDK}/qrcode/include/)
-target_include_directories(qrcode
-                           PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
+target_include_directories(
+  qrcode PUBLIC ${BOLOS_SDK}/include/ ${BOLOS_SDK}/qrcode/include/
+                ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
--- fuzzing/mock/mock.cmake
+++ fuzzing/mock/mock.cmake
@@ -1,3 +1,17 @@
+# -----------------------------------------------------------------------------------
+# IMPORTANT NOTE:
+#
+# When globbing source files from the mock directory, it is **critical** that the
+# "custom" folder appears **alphabetically before** the "generated" folder.
+#
+# This is because the symbols are **statically linked**, and the linker
+# resolves symbols in the **order they appear** in the source list. By ensuring that
+# the custom implementations come first, we allow them to **override** the
+# symbols from the generated mocks.
+#
+# Failing to maintain this order may result in the linker choosing the wrong symbol,
+# and using the generated mocks instead of the custom ones.
+# -----------------------------------------------------------------------------------
 include_guard()
 
 include(${BOLOS_SDK}/fuzzing/macros/macros.cmake)
@@ -5,7 +19,6 @@ include(${BOLOS_SDK}/fuzzing/libs/lib_nbgl.cmake)
 include(${BOLOS_SDK}/fuzzing/libs/lib_standard_app.cmake)
 include(${BOLOS_SDK}/fuzzing/libs/lib_cxng.cmake)
 
-# Commented lines need os mock or depend of another function that needs a mock
 file(
   GLOB
   LIB_MOCK_SOURCES
@@ -13,19 +26,16 @@ file(
   "${BOLOS_SDK}/src/ledger_assert.c"
   "${BOLOS_SDK}/src/cx_wrappers.c"
   "${BOLOS_SDK}/src/cx_hash_iovec.c"
-  "${BOLOS_SDK}/fuzzing/mock/custom/os_task.c"
-  "${BOLOS_SDK}/fuzzing/mock/custom/main.c"
-  "${BOLOS_SDK}/fuzzing/mock/custom/pic.c"
-  "${BOLOS_SDK}/fuzzing/mock/custom/custom_syscalls.c"
-  "${BOLOS_SDK}/fuzzing/mock/generated/generated_syscalls.c")
+  "${BOLOS_SDK}/fuzzing/mock/custom/*.c"
+  "${BOLOS_SDK}/fuzzing/mock/generated/*.c")
 
 add_library(mock ${LIB_MOCK_SOURCES})
 target_link_libraries(mock PUBLIC macros cxng nbgl standard_app)
 target_compile_options(mock PUBLIC ${COMPILATION_FLAGS})
-target_include_directories(mock PUBLIC ${BOLOS_SDK}/lib_cxng/include/)
-target_include_directories(mock PUBLIC ${BOLOS_SDK}/lib_standard_app/)
-target_include_directories(mock PUBLIC ${BOLOS_SDK}/include/)
-target_include_directories(mock PUBLIC ${BOLOS_SDK}/fuzzing/mock/written/)
-target_include_directories(mock PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/)
-target_include_directories(mock
-                           PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include/)
+target_include_directories(mock PUBLIC ${BOLOS_SDK}/lib_cxng/include/
+                                        ${BOLOS_SDK}/target/${TARGET_DEVICE}/include/
+                                        ${BOLOS_SDK}/lib_standard_app/
+                                        ${BOLOS_SDK}/include/
+                                        ${BOLOS_SDK}/fuzzing/mock/written/
+                                        ${BOLOS_SDK}/target/${TARGET_DEVICE}/
+)
