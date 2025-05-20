24b61832031bddeaebd0b51fe3ead7de1a4184ba
nroggeman-ledger <nroggeman-ledger@users.noreply.github.com>
2025-05-20 10:47:51 +0000

    deploy: 67b4737d306ed3af8b48f71df07e471a97ff09aa

--- search/defines_1.js
+++ search/defines_1.js
@@ -1,10 +1,12 @@
 var searchData=
 [
-  ['add64_0',['ADD64',['../cx__utils_8h.html#a4f15fb31bd7bd262691442e10af6d7e2',1,'cx_utils.h']]],
-  ['alloc_5fchunk_5fheader_5fsize_1',['ALLOC_CHUNK_HEADER_SIZE',['../mem__alloc_8c.html#a33e0cf3fef0f595a3fb9b97904a00b19',1,'mem_alloc.c']]],
-  ['app_5fdescription_5fmax_5flen_2',['APP_DESCRIPTION_MAX_LEN',['../nbgl__use__case_8h.html#a8972d4a69d40e4efd9c7437a46d7c8b7',1,'nbgl_use_case.h']]],
-  ['arch_5flittle_5fendian_3',['ARCH_LITTLE_ENDIAN',['../lcx__common_8h.html#a1c8ed4b72dac6929d882e2aeb0261081',1,'lcx_common.h']]],
-  ['array_5flength_4',['ARRAY_LENGTH',['../macros_8h.html#a7539f9740177f669ce68a77ae64f38b5',1,'macros.h']]],
-  ['assign64_5',['ASSIGN64',['../cx__utils_8h.html#a660b5a3766209ed109e4508c6b6c5521',1,'cx_utils.h']]],
-  ['available_5fwidth_6',['AVAILABLE_WIDTH',['../nbgl__layout_8h.html#a7cbe9f98ad52749210b99e1ed27ac2b9',1,'nbgl_layout.h']]]
+  ['action_5fon_5fany_5fbutton_0',['ACTION_ON_ANY_BUTTON',['../nbgl__step_8h.html#a8417d6448a79c4dfc0021f6e68ccfe4b',1,'nbgl_step.h']]],
+  ['action_5fon_5fboth_5fbuttons_1',['ACTION_ON_BOTH_BUTTONS',['../nbgl__step_8h.html#a2dcd4bbbc0b887daf11651e1a67766c7',1,'nbgl_step.h']]],
+  ['add64_2',['ADD64',['../cx__utils_8h.html#a4f15fb31bd7bd262691442e10af6d7e2',1,'cx_utils.h']]],
+  ['alloc_5fchunk_5fheader_5fsize_3',['ALLOC_CHUNK_HEADER_SIZE',['../mem__alloc_8c.html#a33e0cf3fef0f595a3fb9b97904a00b19',1,'mem_alloc.c']]],
+  ['app_5fdescription_5fmax_5flen_4',['APP_DESCRIPTION_MAX_LEN',['../nbgl__use__case_8h.html#a8972d4a69d40e4efd9c7437a46d7c8b7',1,'nbgl_use_case.h']]],
+  ['arch_5flittle_5fendian_5',['ARCH_LITTLE_ENDIAN',['../lcx__common_8h.html#a1c8ed4b72dac6929d882e2aeb0261081',1,'lcx_common.h']]],
+  ['array_5flength_6',['ARRAY_LENGTH',['../macros_8h.html#a7539f9740177f669ce68a77ae64f38b5',1,'macros.h']]],
+  ['assign64_7',['ASSIGN64',['../cx__utils_8h.html#a660b5a3766209ed109e4508c6b6c5521',1,'cx_utils.h']]],
+  ['available_5fwidth_8',['AVAILABLE_WIDTH',['../nbgl__layout_8h.html#a7cbe9f98ad52749210b99e1ed27ac2b9',1,'nbgl_layout.h']]]
 ];
