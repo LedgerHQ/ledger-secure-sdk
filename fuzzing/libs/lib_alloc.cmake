015d716ee552536b70c6a1a3e9a8289c836c4801
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-20 17:46:36 +0200

    Assemble includes and use regex in globs in .cmake

--- fuzzing/libs/lib_alloc.cmake
+++ fuzzing/libs/lib_alloc.cmake
@@ -1,13 +1,11 @@
 include_guard()
 include(${BOLOS_SDK}/fuzzing/macros/macros.cmake)
 
-# Commented lines need os mock or depend of another function that needs a mock
-file(GLOB LIB_ALLOC_SOURCES "${BOLOS_SDK}/lib_alloc/mem_alloc.c")
+file(GLOB LIB_ALLOC_SOURCES "${BOLOS_SDK}/lib_alloc/*.c")
 
 add_library(alloc ${LIB_ALLOC_SOURCES})
 target_link_libraries(alloc PUBLIC macros)
 target_compile_options(alloc PUBLIC ${COMPILATION_FLAGS})
-target_include_directories(alloc PUBLIC ${BOLOS_SDK}/include/)
-target_include_directories(alloc PUBLIC ${BOLOS_SDK}/lib_alloc/)
-target_include_directories(alloc
-                           PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
+target_include_directories(
+  alloc PUBLIC ${BOLOS_SDK}/include/ ${BOLOS_SDK}/lib_alloc/
+               ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
