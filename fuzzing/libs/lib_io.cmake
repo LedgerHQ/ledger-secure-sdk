c56b215163a960b4becd27c1a8a0d16a0f025359
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-20 17:46:36 +0200

    Assemble includes and use regex in globs in .cmake

--- fuzzing/libs/lib_io.cmake
+++ fuzzing/libs/lib_io.cmake
@@ -8,51 +8,37 @@ file(
   GLOB
   LIB_IO_SOURCES
   "${BOLOS_SDK}/src/os_io_usb.c"
-  "${BOLOS_SDK}/src/os_io_seproxyhal.c"
+  "${BOLOS_SDK}/lib_ux_nbgl/ux.c"
   "${BOLOS_SDK}/src/os_io_task.c"
+  "${BOLOS_SDK}/src/os_io_seproxyhal.c"
   "${BOLOS_SDK}/src/ledger_protocol.c"
   "${BOLOS_SDK}/lib_standard_app/io.c"
-  "${BOLOS_SDK}/lib_blewbxx/core/auto/ble_gap_aci.c"
-  "${BOLOS_SDK}/lib_blewbxx/core/auto/ble_gatt_aci.c"
-  "${BOLOS_SDK}/lib_blewbxx/core/auto/ble_hal_aci.c"
-  "${BOLOS_SDK}/lib_blewbxx/core/auto/ble_hci_le.c"
-  "${BOLOS_SDK}/lib_blewbxx/core/auto/ble_l2cap_aci.c"
-  "${BOLOS_SDK}/lib_blewbxx/core/template/osal.c"
-  "${BOLOS_SDK}/lib_blewbxx_impl/src/ledger_ble.c"
-  "${BOLOS_SDK}/lib_stusb/usbd_conf.c"
-  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Core/Src/usbd_core.c"
-  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c"
-  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c"
-  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c"
-  "${BOLOS_SDK}/lib_stusb_impl/u2f_impl.c"
-  "${BOLOS_SDK}/lib_stusb_impl/u2f_io.c"
-  "${BOLOS_SDK}/lib_stusb_impl/usbd_impl.c"
-  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/CCID/src/usbd_ccid_cmd.c"
-  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/CCID/src/usbd_ccid_core.c"
-  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/CCID/src/usbd_ccid_if.c"
-  "${BOLOS_SDK}/lib_ux_nbgl/ux.c")
+  "${BOLOS_SDK}/lib_stusb/*.c"
+  "${BOLOS_SDK}/lib_stusb_impl/*.c"
+  "${BOLOS_SDK}/lib_blewbxx_impl/src/*.c"
+  "${BOLOS_SDK}/lib_blewbxx/core/auto/*.c"
+  "${BOLOS_SDK}/lib_blewbxx/core/template/*.c"
+  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Core/Src/*.c"
+  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/HID/Src/*.c"
+  "${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/CCID/src/*.c")
 
 add_library(io ${LIB_IO_SOURCES})
 target_link_libraries(io PUBLIC nbgl cxng standard_app macros)
 target_compile_options(io PUBLIC ${COMPILATION_FLAGS})
-target_include_directories(io PUBLIC ${BOLOS_SDK}/include/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/)
-target_include_directories(io
-                           PUBLIC ${BOLOS_SDK}/target/${TARGET_DEVICE}/include/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/lib_blewbxx/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/lib_blewbxx/core/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/lib_blewbxx/core/auto/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/lib_blewbxx/core/template/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/lib_blewbxx_impl/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/lib_blewbxx_impl/include/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/lib_stusb/)
 target_include_directories(
-  io PUBLIC ${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Core/Inc)
-target_include_directories(
-  io PUBLIC ${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/HID/Inc/)
-target_include_directories(
-  io PUBLIC ${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/HID/Inc/)
-target_include_directories(
-  io PUBLIC ${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/CCID/inc/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/lib_stusb_impl/)
-target_include_directories(io PUBLIC ${BOLOS_SDK}/)
+  io
+  PUBLIC ${BOLOS_SDK}/include/
+         ${BOLOS_SDK}/target/${TARGET_DEVICE}/
+         ${BOLOS_SDK}/target/${TARGET_DEVICE}/include/
+         ${BOLOS_SDK}/lib_blewbxx/
+         ${BOLOS_SDK}/lib_blewbxx/core/
+         ${BOLOS_SDK}/lib_blewbxx/core/auto/
+         ${BOLOS_SDK}/lib_blewbxx/core/template/
+         ${BOLOS_SDK}/lib_blewbxx_impl/
+         ${BOLOS_SDK}/lib_blewbxx_impl/include/
+         ${BOLOS_SDK}/lib_stusb/
+         ${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Core/Inc
+         ${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/HID/Inc/
+         ${BOLOS_SDK}/lib_stusb/STM32_USB_Device_Library/Class/CCID/inc/
+         ${BOLOS_SDK}/lib_stusb_impl/
+         ${BOLOS_SDK}/)
--- fuzzing/libs/lib_nfc.cmake
+++ fuzzing/libs/lib_nfc.cmake
@@ -1,11 +1,10 @@
 include_guard()
 include(${BOLOS_SDK}/fuzzing/macros/macros.cmake)
 
-# Commented lines need os mock or depend of another function that needs a mock
-file(GLOB LIB_NFC_SOURCES "${BOLOS_SDK}/lib_nfc/src/nfc_ndef.c")
+file(GLOB LIB_NFC_SOURCES "${BOLOS_SDK}/lib_nfc/src/*.c")
 
 add_library(nfc ${LIB_NFC_SOURCES})
 target_link_libraries(nfc PUBLIC macros)
 target_compile_options(nfc PUBLIC ${COMPILATION_FLAGS})
-target_include_directories(nfc PUBLIC ${BOLOS_SDK}/include/)
-target_include_directories(nfc PUBLIC ${BOLOS_SDK}/lib_nfc/include/)
+target_include_directories(nfc PUBLIC ${BOLOS_SDK}/include/
+                                      ${BOLOS_SDK}/lib_nfc/include/)
