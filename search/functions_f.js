71ec5875b66d36a889945ff6b4b3e58c98962bd3
cedelavergne-ledger <cedelavergne-ledger@users.noreply.github.com>
2025-05-22 08:25:57 +0000

    deploy: d6cbad30d65ee8dccdf9936fe939665a866fb523

--- search/functions_f.js
+++ search/functions_f.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['standalone_5fapp_5fmain_0',['standalone_app_main',['../main_8c.html#aab387f12db726f33ee15b00dc7112eba',1,'main.c']]],
+  ['standalone_5fapp_5fmain_0',['standalone_app_main',['../main_8c.html#aab387f12db726f33ee15b00dc7112eba',1,'standalone_app_main(void):&#160;main.c'],['../main__std__app_8h.html#aab387f12db726f33ee15b00dc7112eba',1,'standalone_app_main(void):&#160;main.c']]],
   ['swap_5fcopy_5ftransaction_5fparameters_1',['swap_copy_transaction_parameters',['../swap__entrypoints_8h.html#a7393debc68c0691aee283864a42c1b38',1,'swap_entrypoints.h']]],
   ['swap_5fhandle_5fcheck_5faddress_2',['swap_handle_check_address',['../swap__entrypoints_8h.html#a55ed26aac4d8f352d7e361513678bf19',1,'swap_entrypoints.h']]],
   ['swap_5fhandle_5fget_5fprintable_5famount_3',['swap_handle_get_printable_amount',['../swap__entrypoints_8h.html#a064dc2780845ddaa9e9feaab672d1688',1,'swap_entrypoints.h']]],
