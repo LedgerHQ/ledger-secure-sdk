2df77bff80db26827c5ca869cabc9ad83112a394
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-27 13:50:23 +0200

    Add and separate compilation and link flags

--- fuzzing/extra/apdu_parser.cmake
+++ fuzzing/extra/apdu_parser.cmake
@@ -7,7 +7,7 @@ include(${BOLOS_SDK}/fuzzing/libs/lib_standard_app.cmake)
 add_executable(fuzz_apdu_parser
                ${BOLOS_SDK}/fuzzing/harness/fuzzer_apdu_parser.c)
 target_compile_options(fuzz_apdu_parser PUBLIC ${COMPILATION_FLAGS})
-target_link_options(fuzz_apdu_parser PUBLIC ${COMPILATION_FLAGS})
+target_link_options(fuzz_apdu_parser PUBLIC ${LINK_FLAGS})
 
 # Link with required libraries
 target_link_libraries(fuzz_apdu_parser PUBLIC standard_app)
