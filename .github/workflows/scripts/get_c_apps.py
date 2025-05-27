6612e5a33eef933efe694dd8dc511f8dda7c978b
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2025-05-27 16:16:06 +0200

    Fix linter

--- .github/workflows/scripts/get_c_apps.py
+++ .github/workflows/scripts/get_c_apps.py
@@ -1,7 +1,7 @@
-from ledgered.github import GitHubLedgerHQ, NoManifestException, Condition
-from github.GithubException import GithubException
 import sys
 import json
+from ledgered.github import GitHubLedgerHQ, NoManifestException, Condition  # type: ignore
+from github.GithubException import GithubException
 
 if len(sys.argv) != 2:
     print("Usage: get_c_apps.py <github_token>")
@@ -28,11 +28,13 @@ for app in apps:
         # Filter out apps that are C based
         if manifest.app.sdk == "c":
             if app.name not in excluded_apps:
-                c_apps.append({"app-name": app.name, "build-directory": str(manifest.app.build_directory) ,"devices": manifest.app.devices.json})
+                c_apps.append({"app-name": app.name,
+                               "build-directory": str(manifest.app.build_directory),
+                               "devices": manifest.app.devices.json})
 
 # Print the number of apps to build
 print("Nb of apps to build: ", len(c_apps))
 
 # save the list of apps to build in a json format:
-with open("c_apps.json", "w") as f:
+with open("c_apps.json", "w", encoding="utf-8") as f:
     f.write(json.dumps(c_apps))
