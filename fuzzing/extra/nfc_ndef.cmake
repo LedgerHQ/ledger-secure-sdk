cae333912d571c79a90b067ce2c144be315e9909
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-05-27 13:50:23 +0200

    Add and separate compilation and link flags

--- fuzzing/extra/nfc_ndef.cmake
+++ fuzzing/extra/nfc_ndef.cmake
@@ -6,7 +6,7 @@ include(${BOLOS_SDK}/fuzzing/libs/lib_nfc.cmake)
 # Define the executable and its properties
 add_executable(fuzz_nfc_ndef ${BOLOS_SDK}/fuzzing/harness/fuzzer_nfc_ndef.c)
 target_compile_options(fuzz_nfc_ndef PUBLIC ${COMPILATION_FLAGS})
-target_link_options(fuzz_nfc_ndef PUBLIC ${COMPILATION_FLAGS})
+target_link_options(fuzz_nfc_ndef PUBLIC ${LINK_FLAGS})
 
 # Link with required libraries
 target_link_libraries(fuzz_nfc_ndef PUBLIC nfc)
--- fuzzing/extra/qrcodegen.cmake
+++ fuzzing/extra/qrcodegen.cmake
@@ -7,7 +7,7 @@ include(${BOLOS_SDK}/fuzzing/libs/lib_qrcode.cmake)
 # Define the executable and its properties
 add_executable(fuzz_qrcodegen ${BOLOS_SDK}/fuzzing/harness/fuzzer_qrcodegen.c)
 target_compile_options(fuzz_qrcodegen PUBLIC ${COMPILATION_FLAGS})
-target_link_options(fuzz_qrcodegen PUBLIC ${COMPILATION_FLAGS})
+target_link_options(fuzz_qrcodegen PUBLIC ${LINK_FLAGS})
 
 # Link with required libraries
 target_link_libraries(fuzz_qrcodegen PUBLIC qrcode mock)
