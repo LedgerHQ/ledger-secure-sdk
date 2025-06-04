b74f10ad719ea0ebb6027cc5c958ba8eb67ca7af
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-06-04 15:04:10 +0200

    Move functions to app framework fuzzing  mock
    (cherry picked from commit 304ac828de835ca03d7a09c3e7371cfafa0f0dac)

--- fuzzing/mock/custom/os_task.c
+++ fuzzing/mock/custom/os_task.c
@@ -2,24 +2,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-try_context_t *try_context_get(void);
-
-try_context_t *try_context_set(try_context_t *context);
-
-static try_context_t *G_exception_context = NULL;
-
-try_context_t *try_context_get(void)
-{
-    return G_exception_context;
-}
-
-try_context_t *try_context_set(try_context_t *context)
-{
-    try_context_t *previous = G_exception_context;
-    G_exception_context     = context;
-    return previous;
-}
-
 struct {
     uint8_t bss[0xff];
     uint8_t ebss[0xff];
