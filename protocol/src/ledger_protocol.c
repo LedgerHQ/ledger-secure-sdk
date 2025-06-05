d78f1b86cf9bf08754bdb076980cab8930be49a8
Thomas Joly <thomas.joly@ledger.fr>
2025-06-05 13:47:52 +0200

    [ledger_procotol] fix length check when buffer doesn't fit the chunk

--- protocol/src/ledger_protocol.c
+++ protocol/src/ledger_protocol.c
@@ -219,6 +219,9 @@ void LEDGER_PROTOCOL_tx(ledger_protocol_t *handle,
         if (handle->mtu < tx_chunk_offset) {
             return;
         }
+        if (handle->mtu - tx_chunk_offset > proto_buf_size - tx_chunk_offset) {
+            return;
+        }
         // Remaining buffer length doesn't fit the chunk
         memcpy(&proto_buf[tx_chunk_offset],
                &handle->tx_apdu_buffer[handle->tx_apdu_offset],
