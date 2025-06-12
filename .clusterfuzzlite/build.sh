bfb1563a584d33566c841e867e216e4fbe278552
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-06-12 16:56:51 +0200

    Add ClusterFuzz pr for new fuzzing SDK

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -eu
 
 # build fuzzers
-
+BOLOS_SDK=$(pwd)
+ls
 pushd fuzzing
 rm -rf build/*
-cmake -B build -S .
-make -C build
+cmake -S . -B build -DCMAKE_C_COMPILER=clang -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS:bool=On
+cat build/compile_commands.json
+cmake --build build
 mv ./build/fuzz_* "${OUT}"
 popd
