6b59b3a7e850c356a378241a95f3d12ece53a1af
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-06-17 17:18:52 +0200

    Change build.sh for clusterFuzz
    (cherry picked from commit 188c45fa1b7685950f94b37e7bc10d99420e1180)

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eu
 
 # build fuzzers
-BOLOS_SDK=$(pwd)
-ls
 pushd fuzzing
 rm -rf build/*
-cmake -S . -B build -DCMAKE_C_COMPILER=clang -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS:bool=On
+echo "CC: $CC"
+echo "CFALGS: $CFLAGS"
+cmake -S . -B build -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS:bool=On
 cat build/compile_commands.json
 cmake --build build
 mv ./build/fuzz_* "${OUT}"
