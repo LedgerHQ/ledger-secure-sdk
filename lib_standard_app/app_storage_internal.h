b1fca37314999b264a29a27fbc407ac62a9c900a
Julian Arevalo <julian.arevalo@ledger.fr>
2025-06-17 14:28:42 +0200

    FWEO-1481 app_storage: add CRC

--- lib_standard_app/app_storage_internal.h
+++ lib_standard_app/app_storage_internal.h
@@ -13,18 +13,21 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *****************************************************************************/
-#ifdef HAVE_APP_STORAGE
+#if defined(HAVE_APP_STORAGE) || defined(HAVE_BOLOS)
 
 #pragma once
 
 #include "app_storage.h"
 
-/* The storage consists of the system and the app parts */
+/**
+ * @brief The storage consists of the system and the app parts
+ */
 typedef struct app_storage_s {
+    uint32_t             crc;  ///< CRC32 of the app storage header and data
     app_storage_header_t header;
     uint8_t              data[APP_STORAGE_SIZE];
 } app_storage_t;
 
 void app_storage_init(void);
 
-#endif /* #ifdef HAVE_APP_STORAGE */
+#endif /* HAVE_APP_STORAGE || HAVE_BOLOS */
