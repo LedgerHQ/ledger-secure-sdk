ea1f8f4f7686fcd24a5e4654a5931b241ad8ae3d
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-06-18 09:43:48 +0200

    Test clusterFuzz

--- fuzzing/mock/custom/main.c
+++ fuzzing/mock/custom/main.c
@@ -7,11 +7,11 @@
 /**
  * Variable containing the length of the APDU response to send back.
  */
-//static uint32_t G_output_len = 0;
+// static uint32_t G_output_len = 0;
 
 /**
  * IO state (READY, RECEIVING, WAITING).
  */
-//static io_state_e G_io_state;
+// static io_state_e G_io_state;
 
 bolos_ux_params_t G_ux_params;
