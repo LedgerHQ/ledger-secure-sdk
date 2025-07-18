e6d4149390b9b7ae3e26ce6d4cfcdc7a64b9ea86
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-07-18 16:37:57 +0200

    Add initial corpus to clusterFuzz and documentation

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -e
 
-# build fuzzers
 pushd fuzzing
-cmake -S . -B build -G Ninja
+cmake -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=On
+# Generates .zip for initial corpus in clusterFuzz
+for dir in harness/*; do
+    if [ -d "$dir" ]; then
+        fuzzer_name=$(basename "$dir")
+        zip_name="${fuzzer_name}_seed_corpus.zip"
+        echo "Zipping corpus from $dir into $zip_name"
+
+        (cd "$dir" && zip -q -r "$zip_name" .)
+
+        mv "$dir/$zip_name" "$OUT/"
+    fi
+done
 cmake --build build
 mv ./build/fuzz_* "${OUT}"
 popd
