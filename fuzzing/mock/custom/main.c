f24cdbc4ba84505d42f1f1f6a5b4555b877f1c91
Gustavo Jodar <58124515+Gustavo-Jodar@users.noreply.github.com>
2025-08-04 14:39:37 +0200

    Update main.c

--- fuzzing/mock/custom/main.c
+++ fuzzing/mock/custom/main.c
@@ -4,14 +4,4 @@
 #include "os.h"
 #include "io.h"
 
-/**
- * Variable containing the length of the APDU response to send back.
- */
-// static uint32_t G_output_len = 0;
-
-/**
- * IO state (READY, RECEIVING, WAITING).
- */
-// static io_state_e G_io_state;
-
 bolos_ux_params_t G_ux_params;
