23860b74f78501dce072e3f9da8b99f9e03b46ae
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-08-07 14:17:48 +0200

    generate macros using Makefile

--- fuzzing/macros/macros.cmake
+++ fuzzing/macros/macros.cmake
@@ -17,16 +17,35 @@ if(NOT "${BOLOS_SDK}/fuzzing" STREQUAL ${CMAKE_SOURCE_DIR})
 
   # Building from SDK
 else()
-  if("${TARGET_DEVICE}" STREQUAL "stax")
-    message("${Blue}Importing macros from ${CMAKE_SOURCE_DIR}/macros/macros-stax.txt")
-    file(STRINGS "${CMAKE_SOURCE_DIR}/macros/macros-stax.txt" MACRO_LIST)
+  if(${TARGET_DEVICE} STREQUAL "stax" OR ${TARGET_DEVICE} STREQUAL "flex")
+    # Run `make list-defines` and capture output
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E env BOLOS_SDK=${BOLOS_SDK} TARGET=${TARGET_DEVICE} CC=${CMAKE_C_COMPILER} make list-defines
+      WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/macros"
+      OUTPUT_VARIABLE MACRO_OUTPUT
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
+    )
+
+
+
+
+    if(NOT MACRO_OUTPUT)
+      message(FATAL_ERROR "Failed to extract macros via 'make list-defines'")
+    endif()
+
+    string(REPLACE " " ";" MACRO_LIST "${MACRO_OUTPUT}")
+
+    # Append the special fuzzing macro
     list(APPEND MACRO_LIST "FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION=1")
-    target_compile_definitions(macros INTERFACE ${MACRO_LIST})
-  elseif("${TARGET_DEVICE}" STREQUAL "flex")
-    message("${Blue}Importing macros from ${CMAKE_SOURCE_DIR}/macros/macros-flex.txt")
-    file(STRINGS "${CMAKE_SOURCE_DIR}/macros/macros-flex.txt" MACRO_LIST)
-    list(APPEND MACRO_LIST "FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION=1")
+    list(REMOVE_ITEM MACRO_LIST "PRINTF\\(...\\)=")
+    list(REMOVE_ITEM MACRO_LIST "USE_OS_IO_STACK")
+
     target_compile_definitions(macros INTERFACE ${MACRO_LIST})
   endif()
-
 endif()
+
+message("${Blue}Loaded macros from 'make list-defines' (${CMAKE_SOURCE_DIR}/macros):")
+foreach(M ${MACRO_LIST})
+  message("  - ${M}")
+endforeach()
