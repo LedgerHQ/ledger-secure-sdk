60e60ae8f9efd9a97f0ca60d2a2c5f54d7b348a1
Francois Beutin <francois.beutin@ledger.fr>
2025-08-21 16:42:34 +0200

    Apply sdk formatting

--- lib_standard_app/tlv/tlv_use_case_dynamic_descriptor.h
+++ lib_standard_app/tlv/tlv_use_case_dynamic_descriptor.h
@@ -6,9 +6,9 @@
 #define MAX_TICKER_SIZE 32
 
 typedef struct tlv_dynamic_descriptor_out_s {
-    uint8_t version;
+    uint8_t  version;
     uint32_t coin_type;
-    uint8_t magnitude;
+    uint8_t  magnitude;
     buffer_t TUID;
     // 0 copy is inconvenient for strings because they are not '\0' terminated in the TLV reception
     // format
@@ -16,13 +16,13 @@ typedef struct tlv_dynamic_descriptor_out_s {
 } tlv_dynamic_descriptor_out_t;
 
 typedef enum tlv_dynamic_descriptor_status_e {
-    TLV_DYNAMIC_DESCRIPTOR_SUCCESS = 0,
-    TLV_DYNAMIC_DESCRIPTOR_PARSING_ERROR = 1,
-    TLV_DYNAMIC_DESCRIPTOR_MISSING_STRUCTURE_TAG = 2,
-    TLV_DYNAMIC_DESCRIPTOR_WRONG_TYPE = 3,
-    TLV_DYNAMIC_DESCRIPTOR_MISSING_TAG = 4,
+    TLV_DYNAMIC_DESCRIPTOR_SUCCESS                = 0,
+    TLV_DYNAMIC_DESCRIPTOR_PARSING_ERROR          = 1,
+    TLV_DYNAMIC_DESCRIPTOR_MISSING_STRUCTURE_TAG  = 2,
+    TLV_DYNAMIC_DESCRIPTOR_WRONG_TYPE             = 3,
+    TLV_DYNAMIC_DESCRIPTOR_MISSING_TAG            = 4,
     TLV_DYNAMIC_DESCRIPTOR_WRONG_APPLICATION_NAME = 5,
-    TLV_DYNAMIC_DESCRIPTOR_UNKNOWN_VERSION = 6,
+    TLV_DYNAMIC_DESCRIPTOR_UNKNOWN_VERSION        = 6,
     // Combined with check_signature_with_pki_status_t
     TLV_DYNAMIC_DESCRIPTOR_SIGNATURE_ERROR = 0x80,
 } tlv_dynamic_descriptor_status_t;
