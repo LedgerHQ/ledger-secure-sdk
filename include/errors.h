bfb8fd672a2a19d24d36b9de305eeec72d784f7a
Arthur Bonnaudet <arthur.bonnaudet@ledger.fr>
2025-09-04 16:05:57 +0200

    Make files using #errors.h commpatible with OS build

--- include/errors.h
+++ include/errors.h
@@ -19,6 +19,8 @@
 #if !defined(ERRORS_H)
 #define ERRORS_H
 
+#if !defined(HAVE_BOLOS)
+
 #include "status_words.h"
 
 /**
@@ -124,6 +126,10 @@ enum sdk_generic_identifiers {
 #define SWO_IOL_BLE_0B (ERR_IOL_BLE + ERR_GEN_ID_0B)  // 0x150B
 #define SWO_IOL_BLE_0C (ERR_IOL_BLE + ERR_GEN_ID_0C)  // 0x150C
 #endif                                                // HAVE_BLE
+
+#else
+#include "os_errors.h"
+#endif  // HAVE_BOLOS
 
 // Legacy
 #define EXCEPTION             0x1   // keep original value // SWO_MUI_UNK_01
