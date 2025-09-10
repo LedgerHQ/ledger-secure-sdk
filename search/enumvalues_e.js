6796ba3acd2cb32cbfe2cece97b784331d4bcff5
nroggeman-ledger <nroggeman-ledger@users.noreply.github.com>
2025-09-10 09:22:36 +0000

    deploy: 0574cf327290119f4254ac52d8a05b09ad6cbd86

--- search/enumvalues_e.js
+++ search/enumvalues_e.js
@@ -5,7 +5,7 @@ var searchData=
   ['qrcode_5fv10_2',['QRCODE_V10',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5ea33057fff9b323ff98bad2607e11a68c5',1,'nbgl_types.h']]],
   ['qrcode_5fv4_3',['QRCODE_V4',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5eaaba7b99e456d78a55db9e814b3790cd5',1,'nbgl_types.h']]],
   ['qrcode_5fv4_5fsmall_4',['QRCODE_V4_SMALL',['../nbgl__types_8h.html#a4485b43c167477b5443b771571f16c5ea98744275ffca217fc8f07722dac6f75c',1,'nbgl_types.h']]],
-  ['qrcode_5fwarning_5',['QRCODE_WARNING',['../nbgl__use__case_8h.html#ae8733047730d71c4601798d7688434f7a4a063b15c4b201a811dc606c075a72b6',1,'nbgl_use_case.h']]],
+  ['qrcode_5fwarning_5',['QRCODE_WARNING',['../nbgl__use__case_8h.html#a03a5d1f24c2bc6235d870b2383ff3069a4a063b15c4b201a811dc606c075a72b6',1,'nbgl_use_case.h']]],
   ['quit_5fapp_5ftext_6',['QUIT_APP_TEXT',['../nbgl__page_8h.html#afb48cdeef2dbc001c1091fdfc2c95d3da0a1f25f93a03429f08a0892f9fc8a2e9',1,'nbgl_page.h']]],
   ['quit_5ficon_7',['QUIT_ICON',['../nbgl__page_8h.html#afb48cdeef2dbc001c1091fdfc2c95d3da4d91c622c0c89a27564d1302b7d2b3fc',1,'nbgl_page.h']]]
 ];
