f5a02196e9137f23b2e4d25f0b8d691a47109d14
sra <sandra.rasoamiaramanana@ledger.fr>
2025-09-15 14:32:52 +0200

    Add BLS12-377 curve

--- include/ox_ec.h
+++ include/ox_ec.h
@@ -205,6 +205,8 @@ enum cx_curve_e {
     CX_CURVE_Stark256 = 0x51,
 #endif
 
+    CX_CURVE_BLS12_377_G1 = 0x52,
+
     /** High limit (not included) of Weierstrass curve ID */
     CX_CURVE_WEIERSTRASS_END = 0x6F,
 
