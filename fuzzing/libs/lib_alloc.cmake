104995010d6496d7e85a0baac1b61d4370646d18
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-10-01 16:32:56 +0200

    remove TARGET_DEVICE to use .target file

--- fuzzing/libs/lib_alloc.cmake
+++ fuzzing/libs/lib_alloc.cmake
@@ -7,5 +7,5 @@ add_library(alloc ${LIB_ALLOC_SOURCES})
 target_link_libraries(alloc PUBLIC macros)
 target_compile_options(alloc PUBLIC ${COMPILATION_FLAGS})
 target_include_directories(
-  alloc PUBLIC ${BOLOS_SDK}/include/ ${BOLOS_SDK}/lib_alloc/
-               ${BOLOS_SDK}/target/${TARGET_DEVICE}/include)
+  alloc PUBLIC "${BOLOS_SDK}/include/" "${BOLOS_SDK}/lib_alloc/"
+               "${BOLOS_SDK}/target/${TARGET}/include")
