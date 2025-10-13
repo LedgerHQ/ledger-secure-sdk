874a4cd57846ff02f298005089aee377b79030a3
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-10-13 17:27:29 +0200

    Initialize canary on os_io_init, and check it on receive/sends.
    This restores a behaviour similar to how it used to work in the
    old io_exchange: all apps call os_io_init in the preamble (the
    legacy code used io_seproxyhal_init instead), and we check the
    canary every time the io send/receive functions are called.
    
    Since os_io_init, os_io_rx_evt and os_io_tx_cmd in os_io.c are
    only used if USE_OS_IO_STACK is not defined, the same
    initialization and checks are also implemented for the analogous
    weak functions defined in syscalls.c.

--- io/src/os_io.c
+++ io/src/os_io.c
@@ -67,6 +67,10 @@ uint8_t G_io_init_syscall;
 
 /* Private variables ---------------------------------------------------------*/
 
+#ifdef HAVE_BOLOS_APP_STACK_CANARY
+extern unsigned int app_stack_canary;
+#endif  // HAVE_BOLOS_APP_STACK_CANARY
+
 /* Private functions ---------------------------------------------------------*/
 #ifndef USE_OS_IO_STACK
 static int process_itc_event(uint8_t *buffer_in, size_t buffer_in_length)
@@ -136,6 +140,10 @@ static int process_itc_event(uint8_t *buffer_in, size_t buffer_in_length)
 #ifndef USE_OS_IO_STACK
 int os_io_init(os_io_init_t *init)
 {
+#ifdef HAVE_BOLOS_APP_STACK_CANARY
+    app_stack_canary = APP_STACK_CANARY_MAGIC;
+#endif  // HAVE_BOLOS_APP_STACK_CANARY
+
     if (!init) {
         return -1;
     }
@@ -240,6 +248,13 @@ int os_io_rx_evt(unsigned char *buffer,
 {
     int      status = 0;
     uint16_t length = 0;
+
+#ifdef HAVE_BOLOS_APP_STACK_CANARY
+    // if the canary is corrupted, reset the device
+    if (app_stack_canary != APP_STACK_CANARY_MAGIC) {
+        os_reset();
+    }
+#endif
 
     if (!G_io_seph_buffer_size) {
         status = os_io_seph_se_rx_event(G_io_seph_buffer,
@@ -352,6 +367,13 @@ int os_io_tx_cmd(uint8_t                     type,
                  unsigned short              length,
                  unsigned int               *timeout_ms)
 {
+#ifdef HAVE_BOLOS_APP_STACK_CANARY
+    // if the canary is corrupted, reset the device
+    if (app_stack_canary != APP_STACK_CANARY_MAGIC) {
+        os_reset();
+    }
+#endif
+
     int status = 0;
     switch (type) {
 #ifdef HAVE_IO_USB
--- src/syscalls.c
+++ src/syscalls.c
@@ -39,6 +39,10 @@
 #include "os_endorsement.h"
 #include <string.h>
 
+#ifdef HAVE_BOLOS_APP_STACK_CANARY
+extern unsigned int app_stack_canary;
+#endif  // HAVE_BOLOS_APP_STACK_CANARY
+
 unsigned int SVC_Call(unsigned int syscall_id, void *parameters);
 unsigned int SVC_cx_call(unsigned int syscall_id, unsigned int *parameters);
 
@@ -1865,6 +1869,10 @@ int os_io_seph_se_rx_event(unsigned char *buffer,
 
 __attribute((weak)) int os_io_init(os_io_init_t *init)
 {
+#ifdef HAVE_BOLOS_APP_STACK_CANARY
+    app_stack_canary = APP_STACK_CANARY_MAGIC;
+#endif  // HAVE_BOLOS_APP_STACK_CANARY
+
     unsigned int parameters[1];
     parameters[0] = (unsigned int) init;
     return (int) SVC_Call(SYSCALL_os_io_init_ID, parameters);
@@ -1889,6 +1897,13 @@ __attribute((weak)) int os_io_tx_cmd(unsigned char        type,
                                      unsigned short       length,
                                      unsigned int        *timeout_ms)
 {
+#ifdef HAVE_BOLOS_APP_STACK_CANARY
+    // if the canary is corrupted, reset the device
+    if (app_stack_canary != APP_STACK_CANARY_MAGIC) {
+        os_reset();
+    }
+#endif
+
     unsigned int parameters[4];
     parameters[0] = (unsigned int) type;
     parameters[1] = (unsigned int) buffer;
@@ -1902,6 +1917,13 @@ __attribute((weak)) int os_io_rx_evt(unsigned char *buffer,
                                      unsigned int  *timeout_ms,
                                      bool           check_se_event)
 {
+#ifdef HAVE_BOLOS_APP_STACK_CANARY
+    // if the canary is corrupted, reset the device
+    if (app_stack_canary != APP_STACK_CANARY_MAGIC) {
+        os_reset();
+    }
+#endif
+
     unsigned int parameters[4];
     parameters[0] = (unsigned int) buffer;
     parameters[1] = (unsigned int) buffer_max_length;
