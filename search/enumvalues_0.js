d041ba124678108ec02689e87ae9bcea8d66e54a
nroggeman-ledger <nroggeman-ledger@users.noreply.github.com>
2025-10-21 07:23:53 +0000

    deploy: 05b30a56edadd589ef6fed8a57b38a889502e88e

--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['address_5fbook_5falias_0',['ADDRESS_BOOK_ALIAS',['../nbgl__content_8h.html#aa4402a878699462ea2897df1048f16ebace2c6317f384f361441cdebbf3260baf',1,'nbgl_content.h']]],
   ['anim_5fillustration_1',['ANIM_ILLUSTRATION',['../nbgl__content_8h.html#a264c6a334bd2d1849a4e3e07a598c6d4ad1021d3426067e3c6036a7fdb1b5ba0c',1,'nbgl_content.h']]],
-  ['app_5fctx_2',['APP_CTX',['../nbgl__touch_8c.html#af3520ff6d43011872bab77edd27d4de3a3a7df9667f9b767239268afa893fa06c',1,'nbgl_touch.c']]],
+  ['app_5fctx_2',['APP_CTX',['../nbgl__touch_8c.html#ae8a3b6a5d0d3244ed73924ab2421a0d0a3a7df9667f9b767239268afa893fa06c',1,'nbgl_touch.c']]],
   ['app_5flogger_3',['APP_LOGGER',['../nbgl__debug_8h.html#a726ca809ffd3d67ab4b8476646f26635a53109ee21c43dd0d246e86d9f0aa30cb',1,'nbgl_debug.h']]]
 ];
