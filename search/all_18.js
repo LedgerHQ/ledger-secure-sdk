9f92d5947e394cf1da8117a4a8ffd5cb0e3bcc2f
fbeutin-ledger <fbeutin-ledger@users.noreply.github.com>
2025-11-13 09:46:24 +0000

    deploy: cbb09140c34e4ea93978e00ecc272bc9cfea4083

--- search/all_18.js
+++ search/all_18.js
@@ -21,7 +21,7 @@ var searchData=
   ['withleftborder_18',['withleftborder',['../structnbgl__layoutInternal__s.html#aca90ded9831d0d7463d6ff61d3084574',1,'nbgl_layoutInternal_s::withLeftBorder'],['../structnbgl__layoutDescription__s.html#aa40cd6911a9007efa972e94fe7a4eae5',1,'nbgl_layoutDescription_s::withLeftBorder']]],
   ['withpageindicator_19',['withPageIndicator',['../structnbgl__layoutNavigationBar__t.html#a52b23e5a8baf19e806874e96ff749908',1,'nbgl_layoutNavigationBar_t']]],
   ['withseparationline_20',['withSeparationLine',['../structnbgl__layoutNavigationBar__t.html#ad4d7c86e9b714026349b76e1925636dd',1,'nbgl_layoutNavigationBar_t']]],
-  ['wrapping_21',['wrapping',['../structnbgl__contentTagValueList__s.html#a77507d20647314a449c2f71ff677215e',1,'nbgl_contentTagValueList_s::wrapping'],['../structnbgl__text__area__s.html#a266bbb84e04e918620a74b5269e89c68',1,'nbgl_text_area_s::wrapping']]],
+  ['wrapping_21',['wrapping',['../structnbgl__contentTagValueList__s.html#a77507d20647314a449c2f71ff677215e',1,'nbgl_contentTagValueList_s::wrapping'],['../structnbgl__text__area__s.html#a605816d9bc663b2f0db88947534a72f1',1,'nbgl_text_area_s::wrapping']]],
   ['write_2ec_22',['write.c',['../write_8c.html',1,'']]],
   ['write_2eh_23',['write.h',['../write_8h.html',1,'']]],
   ['write_5fu16_5fbe_24',['write_u16_be',['../write_8h.html#abd61878eb96ee03829d0aaf7428f5562',1,'write_u16_be(const uint8_t *ptr, size_t offset, uint16_t value):&#160;write.h'],['../write_8c.html#a53a796149717dd6cce3039d4ea355829',1,'write_u16_be(uint8_t *ptr, size_t offset, uint16_t value):&#160;write.c']]],
