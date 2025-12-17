2b8a1eb44ef8c4d54ff7a699bcebefaeafbc03d2
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2025-12-17 09:13:07 +0100

    Adapt fuzzing

--- fuzzing/extra/lib_alloc.cmake
+++ fuzzing/extra/lib_alloc.cmake
@@ -13,3 +13,11 @@ target_link_options(fuzz_alloc PUBLIC ${LINK_FLAGS})
 
 # Link with required libraries
 target_link_libraries(fuzz_alloc PUBLIC alloc nbgl io mock)
+
+# Add fuzzer for alloc_utils high-level functions
+add_executable(fuzz_alloc_utils ${BOLOS_SDK}/fuzzing/harness/fuzzer_alloc_utils.c)
+target_compile_options(fuzz_alloc_utils PUBLIC ${COMPILATION_FLAGS})
+target_link_options(fuzz_alloc_utils PUBLIC ${LINK_FLAGS})
+
+# Link with required libraries
+target_link_libraries(fuzz_alloc_utils PUBLIC alloc nbgl io mock)
