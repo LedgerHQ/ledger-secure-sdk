6a476d9396cba1fa42a1ba34949118570beb86d8
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2025-12-30 11:58:14 +0100

    AI adding the function 'c_list_insert_before'

--- lib_c_list/c_list.h
+++ lib_c_list/c_list.h
@@ -19,6 +19,7 @@ void   c_list_pop_front(c_list_node_t **list, f_list_node_del func);
 void   c_list_push_back(c_list_node_t **list, c_list_node_t *node);
 void   c_list_pop_back(c_list_node_t **list, f_list_node_del func);
 void   c_list_insert_after(c_list_node_t *ref, c_list_node_t *node);
+void   c_list_insert_before(c_list_node_t **list, c_list_node_t *ref, c_list_node_t *node);
 void   c_list_remove(c_list_node_t **list, c_list_node_t *node, f_list_node_del func);
 void   c_list_clear(c_list_node_t **list, f_list_node_del func);
 void   c_list_sort(c_list_node_t **list, f_list_node_cmp func);
