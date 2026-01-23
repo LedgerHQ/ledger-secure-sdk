c60cd6563debadf2c783078a21abc44286445965
sra <sandra.rasoamiaramanana@ledger.fr>
2026-01-23 17:51:46 +0100

    Add BLS12_377 derivation mode

--- include/os_hdkey.h
+++ include/os_hdkey.h
@@ -11,15 +11,32 @@
 #include <stdint.h>
 #include <stddef.h>
 #include "decorators.h"
+#include "os_types.h"
 
 /*********************
  *      DEFINES
  *********************/
 
-
 /**********************
  *      TYPEDEFS
  **********************/
+
+typedef enum HDKEY_derive_auth_e {
+    HDKEY_DERIVE_AUTH_256K1 = 0x01u,
+    HDKEY_DERIVE_AUTH_256R1 = 0x02u,
+    HDKEY_DERIVE_AUTH_ED25519 = 0x04u,
+    HDKEY_DERIVE_AUTH_SLIP21 = 0x08u,
+    HDKEY_DERIVE_AUTH_BLS12381G1 = 0x10u,
+    HDKEY_DERIVE_AUTH_BLS12377G1 = 0x20u
+} HDKEY_derive_auth_t;
+
+typedef enum HDKEY_derive_mode_e {
+    HDKEY_DERIVE_MODE_NORMAL = 0x00u,
+    HDKEY_DERIVE_MODE_ED25519_SLIP10  = 0x01u,
+    HDKEY_DERIVE_MODE_SLIP21  = 0x02u,
+    HDKEY_DERIVE_MODE_RESERVED = 0x04u,
+    HDKEY_DERIVE_MODE_BLS12377_ALEO = 0x08u
+} HDKEY_derive_mode_t;
 
 
 /**********************
