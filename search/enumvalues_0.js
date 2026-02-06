c1ad5b22ac20e23ef5f3d655194e14454a99eb34
jarevalo-ledger <jarevalo-ledger@users.noreply.github.com>
2026-02-06 16:30:21 +0000

    deploy: acd1457dd205725b1d8de507ec1ae2cdfab7d3e9

--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['address_5fbook_5falias_0',['ADDRESS_BOOK_ALIAS',['../nbgl__content_8h.html#aa4402a878699462ea2897df1048f16ebace2c6317f384f361441cdebbf3260baf',1,'nbgl_content.h']]],
   ['anim_5fillustration_1',['ANIM_ILLUSTRATION',['../nbgl__content_8h.html#a264c6a334bd2d1849a4e3e07a598c6d4ad1021d3426067e3c6036a7fdb1b5ba0c',1,'nbgl_content.h']]],
-  ['app_5fctx_2',['APP_CTX',['../nbgl__touch_8c.html#ae8a3b6a5d0d3244ed73924ab2421a0d0a3a7df9667f9b767239268afa893fa06c',1,'nbgl_touch.c']]],
-  ['app_5flogger_3',['APP_LOGGER',['../nbgl__debug_8h.html#a726ca809ffd3d67ab4b8476646f26635a53109ee21c43dd0d246e86d9f0aa30cb',1,'nbgl_debug.h']]]
+  ['app_5flogger_2',['APP_LOGGER',['../nbgl__debug_8h.html#a726ca809ffd3d67ab4b8476646f26635a53109ee21c43dd0d246e86d9f0aa30cb',1,'nbgl_debug.h']]]
 ];
