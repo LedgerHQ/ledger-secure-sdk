4bad254203d622dd16de6616ae6c9666662e76ca
Mathias BROUSSET <mathias.brousset@ledger.fr>
2026-02-09 15:57:50 +0100

    Adding missing end of string null byte to metadata following -Wunterminated-string-initialization being addded to -Wextra in clang 21 Adding build flags for migration to clang-21

--- tools/build_clangrt_builtins.sh
+++ tools/build_clangrt_builtins.sh
@@ -36,10 +36,9 @@ sed -i 's/^\(Types: deb\)$/\1 deb-src/g' /etc/apt/sources.list.d/debian.sources
 apt update
 
 apt install -y --no-install-recommends \
-    dpkg-dev \
-    llvm-dev
+    dpkg-dev
 
-LLVM_VERSION=$(clang --version | head -n1 | rev | cut -d" " -f1 | rev)
+LLVM_VERSION="21.1.8~++20251221033036+2078da43e25a"
 LLVM_MAJOR_VERSION=$(echo "$LLVM_VERSION" | cut -d. -f1)
 
 cd /tmp
@@ -59,30 +58,24 @@ cd build
 TARGET=arm-none-eabi
 SYSROOT=/usr/lib/arm-none-eabi
 
-cmake ../compiler-rt \
-    -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
-    -DCOMPILER_RT_OS_DIR="baremetal" \
-    -DCOMPILER_RT_BUILD_BUILTINS=ON \
-    -DCOMPILER_RT_BUILD_CRT=OFF \
-    -DCOMPILER_RT_BUILD_SANITIZERS=OFF \
-    -DCOMPILER_RT_BUILD_XRAY=OFF \
-    -DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
-    -DCOMPILER_RT_BUILD_PROFILE=OFF \
-    -DCOMPILER_RT_BUILD_MEMPROF=OFF \
-    -DCOMPILER_RT_BUILD_ORC=OFF \
-    -DCMAKE_C_COMPILER="$(which clang)" \
-    -DCMAKE_C_COMPILER_TARGET="${TARGET}" \
-    -DCMAKE_ASM_COMPILER_TARGET="${TARGET}" \
-    -DCMAKE_AR="$(which llvm-ar)" \
-    -DCMAKE_NM="$(which llvm-nm)" \
-    -DCMAKE_RANLIB="$(which llvm-ranlib)" \
-    -DCOMPILER_RT_BAREMETAL_BUILD=ON \
-    -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON \
-    -DLLVM_CONFIG_PATH="$(which llvm-config)" \
-    -DCOMPILER_RT_HAS_FPIC_FLAG=OFF \
-    -DCMAKE_C_FLAGS="-mcpu=${TARGET_CPU} -mlittle-endian -mthumb -Oz -g0 -fropi -frwpi" \
-    -DCMAKE_ASM_FLAGS="-mcpu=${TARGET_CPU} -mlittle-endian -mthumb" \
-    -DCMAKE_SYSROOT="$SYSROOT"
+cmake ../compiler-rt/lib/builtins \
+     -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
+     -DCOMPILER_RT_OS_DIR="baremetal" \
+     -DCOMPILER_RT_BUILD_BUILTINS=ON \
+     -DCMAKE_C_COMPILER="$(which clang)" \
+     -DCMAKE_C_COMPILER_TARGET="${TARGET}" \
+     -DCMAKE_ASM_COMPILER_TARGET="${TARGET}" \
+     -DCMAKE_AR="/usr/bin/llvm-ar" \
+     -DCMAKE_NM="/usr/bin/llvm-nm" \
+     -DCMAKE_RANLIB="/usr/bin/llvm-ranlib" \
+     -DCOMPILER_RT_BAREMETAL_BUILD=ON \
+     -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON \
+     -DLLVM_CMAKE_DIR="/usr/lib/llvm-21/lib/cmake/llvm" \
+     -DCOMPILER_RT_HAS_FPIC_FLAG=OFF \
+     -DCMAKE_C_FLAGS="-mcpu=${TARGET_CPU} -mlittle-endian -mthumb -Oz -g0 -fropi -frwpi -fshort-wchar -fshort-enums -ffixed-r9 -mno-movt -isystem ${SYSROOT}/include" \
+     -DCMAKE_ASM_FLAGS="-mcpu=${TARGET_CPU} -mlittle-endian -mthumb -fropi -frwpi -fshort-wchar -fshort-enums -ffixed-r9 -mno-movt " \
+     -DCMAKE_SYSROOT="$SYSROOT"
+
 make -j
 mkdir -p "$OUTPUT_DIR"
 cp lib/baremetal/libclang_rt.builtins-arm.a "$OUTPUT_DIR/libclang_rt.builtins.a"
