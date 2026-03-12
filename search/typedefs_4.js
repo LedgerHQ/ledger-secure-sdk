1dfcf82cfbcbabee5970629918cd1f3deeb3c96f
cedelavergne-ledger <cedelavergne-ledger@users.noreply.github.com>
2026-03-12 10:17:02 +0000

    deploy: 6861f627dd3006aecf2d20d6d3e3435ecf0237e5

--- search/typedefs_4.js
+++ search/typedefs_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['header_5ft_0',['header_t',['../mem__alloc_8c.html#a4eb4dd7e74c119ccddb3032242467d65',1,'mem_alloc.c']]]
+  ['get_5fprintable_5famount_5fparameters_5ft_0',['get_printable_amount_parameters_t',['../swap__lib__calls_8h.html#a36af800bd16616fd12d49b085c6676d7',1,'swap_lib_calls.h']]]
 ];
