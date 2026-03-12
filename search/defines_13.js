ffea1caae33993a2fecde04e1f0a1c91df798fae
fbeutin-ledger <fbeutin-ledger@users.noreply.github.com>
2026-03-12 14:47:34 +0000

    deploy: 5138195f06f4ef1f562a1735ce04d9fd428af91c

--- search/defines_13.js
+++ search/defines_13.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['tag_5fvalue_5farea_5fheight_0',['TAG_VALUE_AREA_HEIGHT',['../nbgl__use__case_8h.html#ab31f847bb29d4cc4f976f4d005a21d48',1,'nbgl_use_case.h']]],
   ['tag_5fvalue_5ficon_5fwidth_1',['TAG_VALUE_ICON_WIDTH',['../nbgl__layout_8c.html#a0b01b8532d837dd5c5196a7d7bdc0c2c',1,'nbgl_layout.c']]],
-  ['tagline_5fpart1_2',['TAGLINE_PART1',['../nbgl__use__case_8h.html#ab71789eb49cda02a662fa20c14cbeb9f',1,'nbgl_use_case.h']]],
-  ['tagline_5fpart2_3',['TAGLINE_PART2',['../nbgl__use__case_8h.html#ac750a561ee227d99c2b5a382fc4d2d93',1,'nbgl_use_case.h']]],
-  ['tempbuffer_4',['tempBuffer',['../nbgl__draw_8c.html#a96cf3c70279d27a64ec03468e1af271d',1,'nbgl_draw.c']]]
+  ['tag_5fvalue_5fintervale_2',['TAG_VALUE_INTERVALE',['../nbgl__layout_8h.html#ad4abf2d384c5ad216c63a06d1222d127',1,'nbgl_layout.h']]],
+  ['tagline_5fpart1_3',['TAGLINE_PART1',['../nbgl__use__case_8h.html#ab71789eb49cda02a662fa20c14cbeb9f',1,'nbgl_use_case.h']]],
+  ['tagline_5fpart2_4',['TAGLINE_PART2',['../nbgl__use__case_8h.html#ac750a561ee227d99c2b5a382fc4d2d93',1,'nbgl_use_case.h']]]
 ];
