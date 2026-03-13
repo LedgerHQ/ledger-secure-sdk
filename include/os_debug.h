a2632d68e91145b3dac2cbb7fb19b04c093e1b06
Nicolas Roggeman <nicolas.roggeman@ledger.fr>
2026-03-13 15:27:01 +0100

    Support Stack consumption APDU, both for Syscall and Application

--- include/os_debug.h
+++ include/os_debug.h
@@ -4,8 +4,12 @@
 #include "decorators.h"
 
 #ifdef DEBUG_OS_STACK_CONSUMPTION
+// possible values for P1
 #define MODE_INITIALIZATION 0x00
 #define MODE_RETRIEVAL      0x01
+// possible values for P2
+#define SYSCALL_STACK_TYPE  0x00
+#define APP_STACK_TYPE      0x01
 /**
  * Debug syscall allowing one to monitor the OS stack usage of other syscalls.
  * This syscall cannot be embedded within the release version of the OS.
