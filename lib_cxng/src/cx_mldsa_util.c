a218c0c63323c1dfbd039e717454bca160492c37
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2026-08-03 16:00:27 +0200

    Fix compilation warning
    (cherry picked from commit 3caca3874880e1f98951c7dfb35e3f0416302dd6)

--- lib_cxng/src/cx_mldsa_util.c
+++ lib_cxng/src/cx_mldsa_util.c
@@ -34,44 +34,63 @@ void MLDSA_UTIL_shake128(uint8_t *out, size_t outlen, const uint8_t *in, size_t 
     cx_shake128_hash(in, inlen, out, outlen);
 }
 
-void MLDSA_UTIL_shake256_two(uint8_t       *out,
-                             size_t         outlen,
-                             const uint8_t *in1,
-                             size_t         in1len,
-                             const uint8_t *in2,
-                             size_t         in2len)
+cx_err_t MLDSA_UTIL_shake256_two(uint8_t       *out,
+                                 size_t         outlen,
+                                 const uint8_t *in1,
+                                 size_t         in1len,
+                                 const uint8_t *in2,
+                                 size_t         in2len)
 {
-    uint8_t buf[256] = {0};
-    size_t  total    = in1len + in2len;
+    uint8_t  buf[256] = {0};
+    size_t   total    = in1len + in2len;
+    cx_err_t error;
 
     if (total <= sizeof(buf)) {
         (void) memcpy(buf, in1, in1len);
         (void) memcpy(buf + in1len, in2, in2len);
-        cx_shake256_hash(buf, total, out, outlen);
+        error = cx_shake256_hash(buf, total, out, outlen);
         explicit_bzero(buf, total);
     }
     else {
         cx_sha3_t ctx = {0};
-        cx_shake256_init_no_throw(&ctx, outlen * 8U);
-        cx_hash_no_throw((cx_hash_t *) &ctx, 0, in1, in1len, NULL, 0);
-        cx_hash_no_throw((cx_hash_t *) &ctx, CX_LAST, in2, in2len, out, outlen);
+        error         = cx_shake256_init_no_throw(&ctx, outlen * 8U);
+        if (error != CX_OK) {
+            return error;
+        }
+        error = cx_hash_no_throw((cx_hash_t *) &ctx, 0, in1, in1len, NULL, 0);
+        if (error != CX_OK) {
+            return error;
+        }
+        error = cx_hash_no_throw((cx_hash_t *) &ctx, CX_LAST, in2, in2len, out, outlen);
     }
+    return error;
 }
 
-void MLDSA_UTIL_shake256_three(uint8_t       *out,
-                               size_t         outlen,
-                               const uint8_t *in1,
-                               size_t         in1len,
-                               const uint8_t *in2,
-                               size_t         in2len,
-                               const uint8_t *in3,
-                               size_t         in3len)
+cx_err_t MLDSA_UTIL_shake256_three(uint8_t       *out,
+                                   size_t         outlen,
+                                   const uint8_t *in1,
+                                   size_t         in1len,
+                                   const uint8_t *in2,
+                                   size_t         in2len,
+                                   const uint8_t *in3,
+                                   size_t         in3len)
 {
     cx_sha3_t ctx = {0};
-    cx_shake256_init_no_throw(&ctx, outlen * 8U);
-    cx_hash_no_throw((cx_hash_t *) &ctx, 0, in1, in1len, NULL, 0);
-    cx_hash_no_throw((cx_hash_t *) &ctx, 0, in2, in2len, NULL, 0);
-    cx_hash_no_throw((cx_hash_t *) &ctx, CX_LAST, in3, in3len, out, outlen);
+    cx_err_t  error;
+
+    error = cx_shake256_init_no_throw(&ctx, outlen * 8U);
+    if (error != CX_OK) {
+        return error;
+    }
+    error = cx_hash_no_throw((cx_hash_t *) &ctx, 0, in1, in1len, NULL, 0);
+    if (error != CX_OK) {
+        return error;
+    }
+    error = cx_hash_no_throw((cx_hash_t *) &ctx, 0, in2, in2len, NULL, 0);
+    if (error != CX_OK) {
+        return error;
+    }
+    return cx_hash_no_throw((cx_hash_t *) &ctx, CX_LAST, in3, in3len, out, outlen);
 }
 
 void MLDSA_UTIL_shake128_seed_nonce(uint8_t       *out,
