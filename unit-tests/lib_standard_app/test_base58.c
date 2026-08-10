e21ab06ce6d6d09de32a3db60e4983e03444f55d
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2026-08-10 10:45:04 +0200

    migrate UT 'lib_standard_app'

--- unit-tests/lib_standard_app/test_base58.c
+++ unit-tests/lib_standard_app/test_base58.c
@@ -1,36 +1,34 @@
-#include <stdarg.h>
-#include <stddef.h>
-#include <setjmp.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
 
-#include <cmocka.h>
+#include "unity.h"
 
 #include "base58.h"
 
-static void test_base58(void **state)
+void test_base58(void)
 {
-    (void) state;
-
     const char in[]           = "USm3fpXnKG5EUBx2ndxBDMPVciP5hGey2Jh4NDv6gmeo1LkMeiKrLJUUBk6Z";
     const char expected_out[] = "The quick brown fox jumps over the lazy dog.";
     uint8_t    out[100]       = {0};
     int        out_len        = base58_decode(in, sizeof(in) - 1, out, sizeof(out));
-    assert_int_equal(out_len, strlen(expected_out));
-    assert_string_equal((char *) out, expected_out);
+    TEST_ASSERT_EQUAL_INT(out_len, strlen(expected_out));
+    TEST_ASSERT_EQUAL_STRING((char *) out, expected_out);
 
     const char in2[]           = "The quick brown fox jumps over the lazy dog.";
     const char expected_out2[] = "USm3fpXnKG5EUBx2ndxBDMPVciP5hGey2Jh4NDv6gmeo1LkMeiKrLJUUBk6Z";
     char       out2[100]       = {0};
     int        out_len2 = base58_encode((uint8_t *) in2, sizeof(in2) - 1, out2, sizeof(out2));
-    assert_int_equal(out_len2, strlen(expected_out2));
-    assert_string_equal((char *) out2, expected_out2);
+    TEST_ASSERT_EQUAL_INT(out_len2, strlen(expected_out2));
+    TEST_ASSERT_EQUAL_STRING((char *) out2, expected_out2);
 }
 
-int main()
-{
-    const struct CMUnitTest tests[] = {cmocka_unit_test(test_base58)};
+void setUp(void) {}
+void tearDown(void) {}
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_base58);
+    return UNITY_END();
 }
